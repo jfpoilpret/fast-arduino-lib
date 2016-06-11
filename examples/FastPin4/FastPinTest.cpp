dfa8f353ab4c2894f6a74ad7b34c1ac667649b70
Jean-François Poilpret <jfpoilpret@gmail.com>
2016-06-12 00:37:48 +0200

    Refactored and simplified example #4.

--- examples/FastPin4/FastPinTest.cpp
+++ examples/FastPin4/FastPinTest.cpp
@@ -4,29 +4,25 @@
  * On Arduino, you should branch LED (in series with 330 Ohm resistors to limit current) on the following pins:
  * - D0-D7
  * i.e. all pins mapped to AVR ATmega328 PORT D
- * Then, you should branch 5 switches (I use DIP switches which are convenient on breadboard):
- * - one side to A0-A3 (LED pattern) or D8 (chase direction)
+ * Then, you should branch 4 switches (I use DIP switches which are convenient on breadboard):
+ * - one side to A0-A2 (number of simultaneously lit LED) and A3 (chase direction)
  * - the other side to GND (we use internal pullup resistors for inputs)
- * 
- * TODO more switches (2) to control speed
  */
 
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <fastarduino/FastPin.hh>
 
-static inline uint8_t calculate_pattern(uint16_t model, uint8_t shift)
+static inline uint8_t shift_pattern(uint8_t pattern, uint8_t shift)
 {
-	//FIXME this does not work when eg pattern is 0xF
-	return (model << shift) >> 4;
+	uint16_t result = (pattern << shift);
+	return result | (result >> 8);
 }
 
-static inline uint16_t calculate_model(uint8_t pattern)
+static inline uint8_t calculate_pattern(uint8_t num_bits)
 {
-	//TODO change model to use pattern as the number of bits to light?
-	uint16_t model = (1 << (pattern + 1)) - 1;
-	return model | (model << 8);
-//	return pattern | (pattern << 8);
+	uint16_t pattern = (1 << (num_bits + 1)) - 1;
+	return pattern;
 }
 
 int main()
@@ -34,18 +30,18 @@ int main()
 	// Enable interrupts at startup time
 	sei();
 	// Prepare ports to read settings and write to LEDs
-	FastPort<_SFR_IO_ADDR(PINC)> PortC{0x00, 0x1F};
+	FastPort<_SFR_IO_ADDR(PINC)> PortC{0x00, 0x0F};
 	FastPort<_SFR_IO_ADDR(PIND)> PortD{0xFF};
 	
 	// Loop of the LED chaser
 	while (true)
 	{
-		volatile uint8_t settings = PortC.get_PIN();
-		uint16_t model = calculate_model(settings & 0x07);
-		bool direction = settings & 0x10;
+		uint8_t settings = PortC.get_PIN();
+		uint8_t pattern = calculate_pattern(settings & 0x07);
+		bool direction = settings & 0x08;
 		for (uint8_t i = 0; i < 8; ++i)
 		{
-			PortD.set_PORT(calculate_pattern(model, (direction ? i : 7 - i)));
+			PortD.set_PORT(shift_pattern(pattern, (direction ? i : 7 - i)));
 			_delay_ms(250.0);
 		}
 	}
