de38729c697bff5b5ab71e50e8fbb067e231a222
Jean-François Poilpret <jfpoilpret@gmail.com>
2016-08-07 21:15:13 +0200

    Cleanup example code.

--- examples/uart/UartApp1/UARTApp1.cpp
+++ examples/uart/UartApp1/UARTApp1.cpp
@@ -22,7 +22,6 @@ int main()
 	sei();
 	
 	// Start UART
-//	AbstractUART uart{Board::USART::USART0, input_buffer, output_buffer};
 	UART<Board::USART::USART0> uart{input_buffer, output_buffer};
 	uart.begin(115200);
 //	uart.begin(230400);
@@ -35,20 +34,11 @@ int main()
 	{
 		out.puts("Enter a letter: ");
 		out.flush();
-//		int input = 123;
 		int input = in.get();
 		out.put(input);
 		out.put('\n');
 		out << (char) input << " " << dec << input << " " << oct << input << " " << hex << input << " " << bin << input << endl;
 		out.flush();
-		//FIXME Why does this delay last ~2'30" instead of just 1"?
-//		_delay_ms(1000.0);
-		// The following shall take 100,000 cycles x 160 = 1 second
-		//TODO Add LED traces here to understand what happens really
-		// What i might be: a forever occurring interrupt that was not cleared?
-		// That seems confirmed by ATmega328P datasheet, p178, $20.6.3, bug seems to be inside 
-		// UART_DataRegisterEmpty method that does not clear DRE interrupt...
-		for (int i = 0; i < 160; ++i)
-			_delay_loop_2(25000);
+		_delay_ms(1000.0);
 	}
 }
