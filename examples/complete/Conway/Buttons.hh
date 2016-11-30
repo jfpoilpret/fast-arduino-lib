dab668655ef6bdfbf76282f1ac33de2356c940c9
Jean-Francois Poilpret <jfpoilpret@gmail.com>
2016-11-30 11:58:10 +0100

    Add buttons support

---
+++ examples/complete/Conway/Buttons.hh
@@ -0,0 +1,50 @@
+#ifndef BUTTONS_HH
+#define BUTTONS_HH
+
+#include <fastarduino/FastIO.hh>
+
+template<Board::Port PORT, uint8_t MASK, uint8_t DEBOUNCE_COUNT>
+class Buttons
+{
+public:
+	Buttons():_latest_state(), _pending_state(), _count() {}
+
+	inline void init()
+	{
+		_port.set_DDR(0);
+		_port.set_PORT(MASK);
+	}
+
+	uint8_t state()
+	{
+		uint8_t state = _port.get_PIN() & MASK;
+		// Don't return state unless it remained the same during DEBOUNCE_COUNT calls
+		if (state == _latest_state)
+		{
+			_pending_state = state;
+			_count = 0;
+		}
+		else if (state == _pending_state)
+		{
+			// Has new state been stable long enough?
+			if (++_count == DEBOUNCE_COUNT)
+				_latest_state = state;
+		}
+		else
+		{
+			// First is a really new state, start counting
+			_pending_state = state;
+			_count = 0;
+		}
+		return _latest_state;
+	}
+	
+private:
+	FastPort<PORT> _port;
+	uint8_t _latest_state;
+	uint8_t _pending_state;
+	uint8_t _count;
+};
+
+#endif /* BUTTONS_HH */
+
