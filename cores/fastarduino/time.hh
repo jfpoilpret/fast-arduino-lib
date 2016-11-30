jfpoilpret/fast-arduino-lib

commit fa1c36ef1bfd467bd29918f34efdf3615ca44cc1

Improve time utilities code size.

cores/fastarduino/time.hh
@@ -2,6 +2,8 @@
 #define	TIME_HH
 
 #include <stdint.h>
+#include <util/delay_basic.h>
+
 #include "utilities.hh"
 
 namespace Time
@@ -22,8 +24,16 @@ namespace Time
 	using MILLIS_PTR = uint32_t (*)();
 	extern MILLIS_PTR millis;
 	
-	void delay_ms(uint16_t ms);
-	void delay_us(uint16_t us);
+	void delay_us(uint16_t us)
+	{
+		_delay_loop_2((us * F_CPU) / 4000000L);
+	}
+
+	void delay_ms(uint16_t ms)
+	{
+		while (ms--) delay_us(1000);
+	}
+
 	void default_delay(uint32_t ms);
 	
 	template<typename CLOCK>

cores/fastarduino/time.cpp
@@ -1,5 +1,3 @@
-#include <util/delay_basic.h>
-
 #include "time.hh"
 #include "Board.hh"
 #include "power.hh"
@@ -30,16 +28,6 @@ uint32_t Time::since(uint32_t start_ms)
 {
 	uint32_t now = Time::millis();
 	return (start_ms <= now ? now - start_ms : 0);
-}
-
-void Time::delay_ms(uint16_t ms)
-{
-	while (ms--) Time::delay_us(1000);
-}
-
-void Time::delay_us(uint16_t us)
-{
-	_delay_loop_2((us * F_CPU) / 4000000L);
 }
 
 void Time::default_delay(uint32_t ms)

commit 361118c438d043c3a323d75b7e2b5afbb182d010

Fix bug introduced in commit [fa1c36e]: potential generation of multiple symbols for Time::delay_us and Time::delay_ms

cores/fastarduino/time.hh
@@ -24,12 +24,14 @@ namespace Time
 	using MILLIS_PTR = uint32_t (*)();
 	extern MILLIS_PTR millis;
 	
-	void delay_us(uint16_t us)
+	inline void delay_us(uint16_t us) INLINE;
+	inline void delay_us(uint16_t us)
 	{
 		_delay_loop_2((us * F_CPU) / 4000000L);
 	}
 
-	void delay_ms(uint16_t ms)
+	inline void delay_ms(uint16_t ms) INLINE;
+	inline void delay_ms(uint16_t ms)
 	{
 		while (ms--) delay_us(1000);
 	}