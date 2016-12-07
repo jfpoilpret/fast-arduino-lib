c28365a6828876fb8d6d2f43f885479df488cd10
Jean-Francois Poilpret <jfpoilpret@gmail.com>
2016-12-07 21:52:31 +0100

    Replace PREVIOUS/NEXT buttons with X/Y potentiometers to ease initial board setup

--- examples/complete/Conway/Conway.cpp
+++ examples/complete/Conway/Conway.cpp
@@ -27,7 +27,7 @@
 #include <util/delay.h>
 
 #include <fastarduino/time.hh>
-//#include <fastarduino/AnalogInput.hh>
+#include <fastarduino/AnalogInput.hh>
 
 #include "Multiplexer.hh"
 #include "Buttons.hh"
@@ -37,8 +37,9 @@ static constexpr const Board::DigitalPin CLOCK = Board::DigitalPin::D2;
 static constexpr const Board::DigitalPin LATCH = Board::DigitalPin::D3;
 static constexpr const Board::DigitalPin DATA = Board::DigitalPin::D4;
 
-static constexpr const Board::DigitalPin PREVIOUS = Board::DigitalPin::D5;
-static constexpr const Board::DigitalPin NEXT = Board::DigitalPin::D6;
+static constexpr const Board::AnalogPin ROW = Board::AnalogPin::A5;
+static constexpr const Board::AnalogPin COLUMN = Board::AnalogPin::A6;
+
 static constexpr const Board::DigitalPin SELECT = Board::DigitalPin::D7;
 static constexpr const Board::DigitalPin START_STOP = Board::DigitalPin::D0;
 
@@ -47,8 +48,9 @@ static constexpr const Board::DigitalPin CLOCK = Board::DigitalPin::D0;
 static constexpr const Board::DigitalPin LATCH = Board::DigitalPin::D1;
 static constexpr const Board::DigitalPin DATA = Board::DigitalPin::D2;
 
-static constexpr const Board::DigitalPin PREVIOUS = Board::DigitalPin::D3;
-static constexpr const Board::DigitalPin NEXT = Board::DigitalPin::D4;
+static constexpr const Board::AnalogPin ROW = Board::AnalogPin::A3;
+static constexpr const Board::AnalogPin COLUMN = Board::AnalogPin::A4;
+
 static constexpr const Board::DigitalPin SELECT = Board::DigitalPin::D5;
 static constexpr const Board::DigitalPin START_STOP = Board::DigitalPin::D6;
 #else
@@ -61,8 +63,6 @@ static constexpr const Board::Port PORT = FastPinType<CLOCK>::PORT;
 // Check at compile time that all pins are on the same port
 static_assert(FastPinType<LATCH>::PORT == PORT, "LATCH must be on same port as CLOCK");
 static_assert(FastPinType<DATA>::PORT == PORT, "DATA must be on same port as CLOCK");
-static_assert(FastPinType<PREVIOUS>::PORT == PORT, "PREVIOUS must be on same port as CLOCK");
-static_assert(FastPinType<NEXT>::PORT == PORT, "NEXT must be on same port as CLOCK");
 static_assert(FastPinType<SELECT>::PORT == PORT, "SELECT must be on same port as CLOCK");
 static_assert(FastPinType<START_STOP>::PORT == PORT, "START_STOP must be on same port as CLOCK");
 
@@ -70,7 +70,6 @@ static_assert(FastPinType<START_STOP>::PORT == PORT, "START_STOP must be on same
 // Multiplexing is done one row every 2ms, ie 8 rows in 16ms
 static constexpr const uint16_t REFRESH_PERIOD_US = 1000;
 // Blinking LEDs are toggled every 20 times the display is fully refreshed (ie 20 x 8 x 2ms = 320ms)
-//FIXME that should be provided as a MS period then counter be deduced!
 static constexpr const uint16_t BLINKING_HALF_TIME_MS = 250;
 static constexpr const uint16_t BLINKING_COUNTER = BLINKING_HALF_TIME_MS * 1000UL / REFRESH_PERIOD_US;
 // Buttons debouncing is done on a duration of 20ms
@@ -85,13 +84,10 @@ using MULTIPLEXER = Matrix8x8Multiplexer<CLOCK, LATCH, DATA, BLINKING_COUNTER>;
 static constexpr const uint8_t ROWS = MULTIPLEXER::ROWS;
 static constexpr const uint8_t COLUMNS = MULTIPLEXER::COLUMNS;
 
-// Calculate direction of pins (3 output, 4 input with pullups)
+// Calculate direction of pins (3 output, 2 input with pullups)
 static constexpr const uint8_t ALL_DDR = MULTIPLEXER::DDR_MASK;
-static constexpr const uint8_t BUTTONS_MASK =	FastPinType<PREVIOUS>::MASK |
-												FastPinType<NEXT>::MASK |
-												FastPinType<SELECT>::MASK |
-												FastPinType<START_STOP>::MASK;
-static constexpr const uint8_t ALL_PORT =	MULTIPLEXER::PORT_MASK | BUTTONS_MASK;
+static constexpr const uint8_t BUTTONS_MASK = FastPinType<SELECT>::MASK | FastPinType<START_STOP>::MASK;
+static constexpr const uint8_t ALL_PORT = MULTIPLEXER::PORT_MASK | BUTTONS_MASK;
 
 //TODO Make it a template based on game size (num rows, num columns)
 //TODO Make a class to hold one generation and access its members?
@@ -164,12 +160,19 @@ int main()
 	//===============================================
 	{
 		Buttons<PORT, BUTTONS_MASK, DEBOUNCE_COUNTER> buttons;
+		AnalogInput<ROW, Board::AnalogReference::AVCC, uint8_t> row_input;
+		AnalogInput<COLUMN, Board::AnalogReference::AVCC, uint8_t> column_input;
 		uint8_t row = 0;
 		uint8_t col = 0;
 		mux.blinks()[0] = _BV(0);
 		uint8_t last_state = 0;
 		while (true)
 		{
+			// Update selected cell
+			mux.blinks()[row] = 0;
+			row = row_input.sample() >> 5;
+			col = column_input.sample() >> 5;
+			mux.blinks()[row] = _BV(col);
 			// Check button states
 			uint8_t state = buttons.state();
 			if (state && state != last_state)
@@ -180,21 +183,6 @@ int main()
 				// If SELECT pushed, then change current LED status
 				if (state & FastPinType<SELECT>::MASK)
 					mux.data()[row] ^= _BV(col);
-				// If NEXT/PREVIOUS then update blink
-				mux.blinks()[row] = 0;
-				if (state & FastPinType<NEXT>::MASK)
-				{
-					row = (row == ROWS - 1 ? 0 : row + 1);
-					if (!row)
-						col = (col == COLUMNS - 1 ? 0 : col + 1);
-				}
-				if (state & FastPinType<PREVIOUS>::MASK)
-				{
-					row = (row == 0 ? ROWS - 1 : row - 1);
-					if (!row)
-						col = (col == 0 ? COLUMNS - 1 : col - 1);
-				}
-				mux.blinks()[row] = _BV(col);
 			}
 			last_state = state;
 			mux.blink();
