4fc6db59ab04ba4bf8ca4990818347a383fc88a3
Jean-Francois Poilpret <jfpoilpret@gmail.com>
2017-01-15 17:21:45 +0100

    Add empty interrupt handler implementation.

--- cores/fastarduino/ExternalInterrupt.hh
+++ cores/fastarduino/ExternalInterrupt.hh
@@ -7,6 +7,15 @@ public:
 	virtual bool on_pin_change() = 0;
 };
 
+class EmptyInterruptHandler: public ExternalInterruptHandler
+{
+public:
+	virtual bool on_pin_change() override
+	{
+		return false;
+	}
+};
+
 //TODO More functional subclasses to:
 // - allow detection of PCI mode (RAISE, LOWER...)
 // - allow chaining PCI handling to several handlers
