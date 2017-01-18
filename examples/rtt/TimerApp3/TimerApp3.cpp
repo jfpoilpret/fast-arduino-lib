3dbe80feb1f47bca3477228f45bcb10a90d97efc
Jean-Francois Poilpret <jfpoilpret@gmail.com>
2017-01-18 21:27:02 +0100

    Rework and fix Timer ISR size optimization.

--- examples/rtt/TimerApp3/TimerApp3.cpp
+++ examples/rtt/TimerApp3/TimerApp3.cpp
@@ -1,6 +1,14 @@
 /*
  * Timer compilation example.
- * TODO
+ * Shows how to use a CTC Timer (not RTT) to blink a LED.
+ * 
+ * Wiring:
+ * - on ATmega328P based boards (including Arduino UNO):
+ *   - D13 (PB5) LED connected to ground through a resistor
+ * - on Arduino MEGA:
+ *   - D13 (PB7) LED connected to ground through a resistor
+ * - on ATtinyX4 based boards:
+ *   - D7 (PA7) LED connected to ground through a resistor
  */
 
 #include <fastarduino/FastIO.hh>
@@ -8,7 +16,7 @@
 
 constexpr const Board::Timer TIMER = Board::Timer::TIMER1;
 // Define vectors we need in the example
-USE_TIMERS(1);
+USE_TIMERS(1)
 
 using TIMER_TYPE = Timer<TIMER>;
 constexpr const uint32_t PERIOD_US = 1000000;
