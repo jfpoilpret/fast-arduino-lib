7e6eeeb1eccc82cef9b3af59f8b440a8bb234bfd
Jean-Francois Poilpret <jfpoilpret@gmail.com>
2017-01-31 22:07:26 +0100

    Simplify library's main.cpp by removing ABI methods that look just useless.

--- cores/fastarduino/main.cpp
+++ cores/fastarduino/main.cpp
@@ -1,31 +1,7 @@
-// Important copyright notice: 
-// Large parts of this file were copied from Mikael Patel's Cosa library, which copyright appears below
-
-/**
- * @file main.cpp
- * @version 1.0
- *
- * @section License
- * Copyright (C) 2013-2015, Mikael Patel
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * This file is part of the Arduino Che Cosa project.
- */
-#include <avr/io.h>
-#include <avr/interrupt.h>
 
 #define UNUSED __attribute__((unused))
 
-int main() __attribute__((weak, OS_main));
+int main() __attribute__((weak));
 int main()
 {
 	return 0;
@@ -34,40 +10,4 @@ int main()
 void exit(int status) __attribute__((weak));
 void exit(int status UNUSED)
 {
-}
-
-/**
- * Support for local static variables
- */
-namespace __cxxabiv1
-{
-	typedef int __guard;
-
-	extern "C" int __cxa_guard_acquire(__guard *g UNUSED)
-	{
-		return (0);
-	}
-
-	extern "C" void __cxa_guard_release(__guard *g UNUSED)
-	{
-	}
-
-	extern "C" void __cxa_guard_abort(__guard *g UNUSED)
-	{
-	}
-
-	extern "C" void __cxa_pure_virtual(void)
-	{
-	}
-
-	void* __dso_handle = 0;
-
-	extern "C" int __cxa_atexit(void (*destructor)(void*) UNUSED, void* arg UNUSED, void* dso UNUSED)
-	{
-		return 0;
-	}
-
-	extern "C" void __cxa_finalize(void* f UNUSED)
-	{
-	}
 }
