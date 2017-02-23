b55b3c3e46911d48a35a670ce391a9300d90e899
Jean-Francois Poilpret <jfpoilpret@gmail.com>
2017-02-23 23:49:27 +0100

    Some experiments with PROGMEM stuff.

--- examples/experiments/isr1/TestIsr.cpp
+++ examples/experiments/isr1/TestIsr.cpp
@@ -9,12 +9,14 @@
 #include <avr/pgmspace.h>
 
 // Utilities to handle PROGMEM storage
-class FlashStorage;
-#define F(ptr) (__extension__({static const char __c[] PROGMEM = (ptr); (const FlashStorage*) &__c[0];}))
+template<typename T> class FlashStorage;
+#define F_(type, ptr) (__extension__({static const type __fs[] PROGMEM = (ptr); (const FlashStorage< type >*) &__fs[0];}))
+//#define F(ptr) (__extension__({static const char __c[] PROGMEM = (ptr); (const FlashStorage*) &__c[0];}))
+#define F(ptr) F_(char, ptr)
 
 using CALLBACK = void (*)(char);
 
-static void f(const FlashStorage* flash, CALLBACK cb)
+static void f(const FlashStorage<char>* flash, CALLBACK cb)
 {
 	uint16_t address = (uint16_t) flash;
 	while (char value = pgm_read_byte(address++)) cb(value);
