4dd6ee2bdd49f214bbc923c91b60e8ccf3e90aa5
Jean-Francois Poilpret <jfpoilpret@gmail.com>
2017-02-25 00:32:34 +0100

    Add new example to help debug input streams.

---
+++ examples/uart/UartApp6/UARTApp6.cpp
@@ -0,0 +1,81 @@
+//   Copyright 2016-2017 Jean-Francois Poilpret
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+/*
+ * Hardware UART example.
+ * This program demonstrates usage of FastArduino Hardware UART support (on target supporting it) and formatted
+ * input streams.
+ * 
+ * Wiring:
+ * - on Arduino UNO and Arduino MEGA:
+ *   - Use standard TX/RX
+ * - on ATmega328P based boards:
+ *   - Use standard TX/RX connected to an Serial-USB converter
+ * - on ATtinyX4 based boards:
+ *   - NOT SUPPORTED
+ */
+
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include <fastarduino/uart.h>
+
+// Define vectors we need in the example
+REGISTER_UART_ISR(0)
+
+// Buffers for UART
+static const uint8_t INPUT_BUFFER_SIZE = 64;
+static const uint8_t OUTPUT_BUFFER_SIZE = 64;
+static char input_buffer[INPUT_BUFFER_SIZE];
+static char output_buffer[OUTPUT_BUFFER_SIZE];
+
+using INPUT = FormattedInput<InputBuffer>;
+using OUTPUT = FormattedOutput<OutputBuffer>;
+
+template<typename T>
+static void handle(OUTPUT& out, INPUT& in, const FlashStorage* type)
+{
+	out << type << F(": ") << flush;
+	T value;
+	in >> value;
+	out << value << endl << flush;
+}
+
+int main() __attribute__((OS_main));
+int main()
+{
+	// Enable interrupts at startup time
+	sei();
+	
+	// Start UART
+	UART<Board::USART::USART0> uart{input_buffer, output_buffer};
+	uart.register_handler();
+	uart.begin(115200);
+	FormattedInput<InputBuffer> in = uart.fin();
+	FormattedOutput<OutputBuffer> out = uart.fout();
+
+	// Event Loop
+	while (true)
+	{
+		//TODO try all formatted inputs
+		handle<char>(out, in, F("char"));
+		handle<uint16_t>(out, in, F("uint16_t"));
+		handle<int16_t>(out, in, F("int16_t"));
+		handle<uint32_t>(out, in, F("uint32_t"));
+		handle<int32_t>(out, in, F("int32_t"));
+		handle<bool>(out, in, F("bool"));
+		
+		_delay_ms(1000.0);
+	}
+}
