8858559311f5a4ec8dd60258cfda4025e988376e
Jean-Francois Poilpret <jfpoilpret@gmail.com>
2017-03-05 22:27:06 +0100

    PWM: port traits to MEGA.

--- examples/experiments/isr1/TestIsr.cpp
+++ examples/experiments/isr1/TestIsr.cpp
@@ -4,14 +4,13 @@
  * It does not do anything interesting as far as hardware is concerned.
  */
 
-//#include <fastarduino/fast_io.h>
 #include <fastarduino/time.h>
 #include <fastarduino/timer.h>
 #include <fastarduino/pwm.h>
 
 // Define here which PWM mode you want to test, and which timer you want to use
-#define FASTPWM 1
-#define TIMER_NUM 0
+#define FASTPWM 0
+#define TIMER_NUM 5
 
 #if TIMER_NUM == 0
 constexpr const board::Timer TIMER = board::Timer::TIMER0;
@@ -19,6 +18,12 @@ constexpr const board::Timer TIMER = board::Timer::TIMER0;
 constexpr const board::Timer TIMER = board::Timer::TIMER1;
 #elif TIMER_NUM == 2
 constexpr const board::Timer TIMER = board::Timer::TIMER2;
+#elif TIMER_NUM == 3
+constexpr const board::Timer TIMER = board::Timer::TIMER3;
+#elif TIMER_NUM == 4
+constexpr const board::Timer TIMER = board::Timer::TIMER4;
+#elif TIMER_NUM == 5
+constexpr const board::Timer TIMER = board::Timer::TIMER5;
 #endif
 using TIMER_TYPE = timer::Timer<TIMER>;
 
@@ -47,14 +52,23 @@ using timer::TimerOutputMode;
 using analog::PWMOutput;
 
 #if TIMER_NUM == 0
-using LED1_PIN = PWMOutput<board::PWMPin::D6_PD6_OC0A>;
-using LED2_PIN = PWMOutput<board::PWMPin::D5_PD5_OC0B>;
+using LED1_PIN = PWMOutput<board::PWMPin::D13_PB7_OC0A>;
+using LED2_PIN = PWMOutput<board::PWMPin::D4_PG5_OC0B>;
 #elif TIMER_NUM == 1
-using LED1_PIN = PWMOutput<board::PWMPin::D9_PB1_OC1A>;
-using LED2_PIN = PWMOutput<board::PWMPin::D10_PB2_OC1B>;
+using LED1_PIN = PWMOutput<board::PWMPin::D11_PB5_OC1A>;
+using LED2_PIN = PWMOutput<board::PWMPin::D12_PB6_OC1B>;
 #elif TIMER_NUM == 2
-using LED1_PIN = PWMOutput<board::PWMPin::D11_PB3_OC2A>;
-using LED2_PIN = PWMOutput<board::PWMPin::D3_PD3_OC2B>;
+using LED1_PIN = PWMOutput<board::PWMPin::D10_PB4_OC2A>;
+using LED2_PIN = PWMOutput<board::PWMPin::D9_PH6_OC2B>;
+#elif TIMER_NUM == 3
+using LED1_PIN = PWMOutput<board::PWMPin::D2_PE4_OC3B>;
+using LED2_PIN = PWMOutput<board::PWMPin::D3_PE5_OC3C>;
+#elif TIMER_NUM == 4
+using LED1_PIN = PWMOutput<board::PWMPin::D6_PH3_OC4A>;
+using LED2_PIN = PWMOutput<board::PWMPin::D7_PH4_OC4B>;
+#elif TIMER_NUM == 5
+using LED1_PIN = PWMOutput<board::PWMPin::D46_PL3_OC5A>;
+using LED2_PIN = PWMOutput<board::PWMPin::D44_PL5_OC5C>;
 #endif
 
 int main()
