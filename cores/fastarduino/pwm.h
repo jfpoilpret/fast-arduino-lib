718c75806775ed535e855e2f6711e7e9e4e43972
Jean-Francois Poilpret <jfpoilpret@gmail.com>
2017-03-05 16:47:21 +0100

    PWM API: add PWMOutput class.

---
+++ cores/fastarduino/pwm.h
@@ -0,0 +1,65 @@
+//   Copyright 2016-2017 Jean-Francois Poilpret
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+#ifndef PWM_H
+#define PWM_H
+
+#include "boards/board.h"
+#include "boards/board_traits.h"
+#include "fast_io.h"
+#include "timer.h"
+
+namespace analog
+{
+	using timer::TimerOutputMode;
+	
+	template<board::DigitalPin PIN>
+	class PWMOutput
+	{
+		using TRAIT = board_traits::PWMPin_trait<PIN>;
+		static constexpr const uint8_t COM = TRAIT::COM;
+		using TIMER_TRAIT = board_traits::Timer_trait<TRAIT::TIMER>;
+		
+	public:
+		using TIMER = timer::Timer<TRAIT::TIMER>;
+		
+		PWMOutput(TIMER& timer, TimerOutputMode output_mode = TimerOutputMode::NON_INVERTING):_timer{timer}
+		{
+			static_assert(TRAIT::HAS_PWM, "PIN must be a PWM pin");
+			// Initialize pin as output
+			typename gpio::FastPinType<PIN>::TYPE output{gpio::PinMode::OUTPUT};
+			// Set com mode for pin
+			_timer.template set_output_mode<COM>(output_mode);
+		}
+		
+		inline void set_output_mode(TimerOutputMode output_mode)
+		{
+			_timer.template set_output_mode<COM>(output_mode);
+		}
+
+		using TYPE = typename TIMER_TRAIT::TYPE;
+		static constexpr const TYPE MAX = TIMER_TRAIT::MAX_PWM;
+		
+		inline void set_duty(TYPE duty)
+		{
+			_timer.template set_max<COM>(duty);
+		}
+		
+	private:
+		TIMER& _timer;
+	};
+}
+
+#endif /* PWM_H */
+
--- examples/experiments/isr1/TestIsr.cpp
+++ examples/experiments/isr1/TestIsr.cpp
@@ -4,13 +4,14 @@
  * It does not do anything interesting as far as hardware is concerned.
  */
 
-#include <fastarduino/fast_io.h>
+//#include <fastarduino/fast_io.h>
 #include <fastarduino/time.h>
 #include <fastarduino/timer.h>
+#include <fastarduino/pwm.h>
 
 // Define here which PWM mode you want to test, and which timer you want to use
-#define FASTPWM 0
-#define TIMER_NUM 2
+#define FASTPWM 1
+#define TIMER_NUM 0
 
 #if TIMER_NUM == 0
 constexpr const board::Timer TIMER = board::Timer::TIMER0;
@@ -34,7 +35,7 @@ using TIMER_TYPE = timer::Timer<TIMER>;
 // Frequency for PWM
 constexpr const uint16_t PWM_FREQUENCY = 450;
 constexpr const TIMER_TYPE::TIMER_PRESCALER PRESCALER = COMPUTE_PWM_PRESCALER(PWM_FREQUENCY);
-//TODO Timer API to check adequacy of prescaler for frequency (within some min/max range)
+// Check the actual PWM frequency we get with this timer
 static_assert(COMPUTE_PWM_FREQUENCY(PRESCALER) >= 450, "PWM Frequency is expected greater than 450");
 static_assert(COMPUTE_PWM_FREQUENCY(PRESCALER) < 2000, "PWM Frequency is expected less than 1000");
 
@@ -43,38 +44,35 @@ constexpr const uint16_t LOOP_DELAY_MS = 1000;
 using time::delay_ms;
 using timer::TimerMode;
 using timer::TimerOutputMode;
-using gpio::FastPinType;
-using gpio::PinMode;
+using analog::PWMOutput;
 
 #if TIMER_NUM == 0
-using LED1_PIN = FastPinType<board::PWMPin::D6_PD6_OC0A>::TYPE;
-using LED2_PIN = FastPinType<board::PWMPin::D5_PD5_OC0B>::TYPE;
+using LED1_PIN = PWMOutput<board::PWMPin::D6_PD6_OC0A>;
+using LED2_PIN = PWMOutput<board::PWMPin::D5_PD5_OC0B>;
 #elif TIMER_NUM == 1
-using LED1_PIN = FastPinType<board::PWMPin::D9_PB1_OC1A>::TYPE;
-using LED2_PIN = FastPinType<board::PWMPin::D10_PB2_OC1B>::TYPE;
+using LED1_PIN = PWMOutput<board::PWMPin::D9_PB1_OC1A>;
+using LED2_PIN = PWMOutput<board::PWMPin::D10_PB2_OC1B>;
 #elif TIMER_NUM == 2
-using LED1_PIN = FastPinType<board::PWMPin::D11_PB3_OC2A>::TYPE;
-using LED2_PIN = FastPinType<board::PWMPin::D3_PD3_OC2B>::TYPE;
+using LED1_PIN = PWMOutput<board::PWMPin::D11_PB3_OC2A>;
+using LED2_PIN = PWMOutput<board::PWMPin::D3_PD3_OC2B>;
 #endif
 
 int main()
 {
-	LED1_PIN led1{PinMode::OUTPUT};
-	LED2_PIN led2{PinMode::OUTPUT};
 	TIMER_TYPE timer{TIMER_MODE};
-	timer.set_output_mode<0>(TimerOutputMode::NON_INVERTING);
-	timer.set_output_mode<1>(TimerOutputMode::NON_INVERTING);
+	LED1_PIN led1{timer};
+	LED2_PIN led2{timer};
 	timer._begin(PRESCALER);
 	sei();
 	
-	TIMER_TYPE::TIMER_TYPE duty1 = 0;
-	TIMER_TYPE::TIMER_TYPE duty2 = TIMER_TYPE::PWM_MAX;
+	LED1_PIN::TYPE duty1 = 0;
+	LED2_PIN::TYPE duty2 = LED2_PIN::MAX;
 	while (true)
 	{
-		timer.set_max<0>(duty1++);
-		timer.set_max<1>(duty2--);
-		if (duty1 > TIMER_TYPE::PWM_MAX) duty1 = 0;
-		if (duty2 > TIMER_TYPE::PWM_MAX) duty2 = TIMER_TYPE::PWM_MAX;
+		led1.set_duty(duty1++);
+		led2.set_duty(duty2--);
+		if (duty1 > LED1_PIN::MAX) duty1 = 0;
+		if (duty2 > LED2_PIN::MAX) duty2 = LED2_PIN::MAX;
 		delay_ms(LOOP_DELAY_MS);
 	}
 }
