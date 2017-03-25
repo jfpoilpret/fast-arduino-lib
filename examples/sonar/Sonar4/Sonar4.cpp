761d9ed266413f25f74944aca12953750940529a
Jean-Francois Poilpret <jfpoilpret@gmail.com>
2017-03-25 19:17:05 +0100

    Add 4th sonar example (2 PCINT pins).

---
+++ examples/sonar/Sonar4/Sonar4.cpp
@@ -0,0 +1,92 @@
+//   Copyright 2016-2017 Jean-Francois Poilpret
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+/*
+ * Asynchronous sonar sensor read and conversion.
+ * This program shows usage of FastArduino HCSR04 device API with PCINT ISR.
+ * 
+ * Wiring: TODO
+ * - on ATmega328P based boards (including Arduino UNO):
+ * - on Arduino MEGA:
+ * - on ATtinyX4 based boards:
+ *   - D1: TX output connected to Serial-USB allowing traces display on a PC terminal
+ */
+
+#include <fastarduino/boards/board.h>
+#include <fastarduino/uart.h>
+#include <fastarduino/gpio.h>
+#include <fastarduino/time.h>
+#include <fastarduino/realtime_timer.h>
+#include <fastarduino/flash.h>
+#include <fastarduino/pci.h>
+#include <fastarduino/devices/hcsr04.h>
+
+static constexpr const board::DigitalPin TRIGGER1 = board::DigitalPin::D2_PD2;
+static constexpr const board::DigitalPin ECHO1 = board::InterruptPin::D3_PD3_PCI2;
+static constexpr const board::DigitalPin TRIGGER2 = board::DigitalPin::D4_PD4;
+static constexpr const board::DigitalPin ECHO2 = board::InterruptPin::D5_PD5_PCI2;
+static constexpr const board::Timer TIMER = board::Timer::TIMER2;
+
+static constexpr const uint8_t OUTPUT_BUFFER_SIZE = 64;
+// Buffers for UART
+static char output_buffer[OUTPUT_BUFFER_SIZE];
+
+using RTT = timer::RTT<TIMER>;
+using PROXIM1 = devices::sonar::HCSR04<TIMER, TRIGGER1, ECHO1>;
+using PROXIM2 = devices::sonar::HCSR04<TIMER, TRIGGER2, ECHO2>;
+using devices::sonar::distance_mm;
+
+// Register all needed ISR
+REGISTER_RTT_ISR(2)
+REGISTER_UATX_ISR(0)
+//TODO 
+//REGISTER_HCSR04_INT_ISR(TIMER, 1, TRIGGER1, ECHO1)		
+
+int main() __attribute__((OS_main));
+int main()
+{
+	sei();
+	
+	serial::hard::UATX<board::USART::USART0> uart{output_buffer};
+	uart.register_handler();
+	uart.begin(115200);
+	auto out = uart.fout();
+	
+	RTT rtt;
+	rtt.register_rtt_handler();
+	rtt.begin();
+	typename interrupt::PCIType<ECHO1>::TYPE signal;
+	//TODO replace with only one call to enable_pins()
+	signal.enable_pin<ECHO1>();
+	signal.enable_pin<ECHO2>();
+	signal.enable();
+	PROXIM1 sensor1{rtt};
+	PROXIM2 sensor2{rtt};
+	
+	out << F("Starting...\n") << streams::flush;
+	
+	while (true)
+	{
+		sensor1.async_echo();
+		sensor2.async_echo();
+		uint16_t pulse1 = sensor1.await_echo_us();
+		uint16_t mm1 = distance_mm(pulse1);
+		uint16_t pulse2 = sensor2.await_echo_us();
+		uint16_t mm2 = distance_mm(pulse2);
+		// trace value to output
+		out << F("Pulse1: ") << pulse1  << F(" us. Distance: ") << mm1 << F(" mm\n") << streams::flush;
+		out << F("Pulse2: ") << pulse2  << F(" us. Distance: ") << mm2 << F(" mm\n") << streams::flush;
+		time::delay_ms(1000);
+	}
+}
---
+++ examples/sonar/Sonar4/nbproject/Makefile-variables.mk
@@ -0,0 +1,51 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# UNO-Release configuration
+CND_PLATFORM_UNO-Release=AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_DIR_UNO-Release=dist/UNO-Release/AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_NAME_UNO-Release=sonar4
+CND_ARTIFACT_PATH_UNO-Release=dist/UNO-Release/AVR-GNU-Toolchain-3.5.3-Linux/sonar4
+CND_PACKAGE_DIR_UNO-Release=dist/UNO-Release/AVR-GNU-Toolchain-3.5.3-Linux/package
+CND_PACKAGE_NAME_UNO-Release=sonar4.tar
+CND_PACKAGE_PATH_UNO-Release=dist/UNO-Release/AVR-GNU-Toolchain-3.5.3-Linux/package/sonar4.tar
+# ATmega328-Release configuration
+CND_PLATFORM_ATmega328-Release=AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_DIR_ATmega328-Release=dist/ATmega328-Release/AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_NAME_ATmega328-Release=sonar4
+CND_ARTIFACT_PATH_ATmega328-Release=dist/ATmega328-Release/AVR-GNU-Toolchain-3.5.3-Linux/sonar4
+CND_PACKAGE_DIR_ATmega328-Release=dist/ATmega328-Release/AVR-GNU-Toolchain-3.5.3-Linux/package
+CND_PACKAGE_NAME_ATmega328-Release=sonar4.tar
+CND_PACKAGE_PATH_ATmega328-Release=dist/ATmega328-Release/AVR-GNU-Toolchain-3.5.3-Linux/package/sonar4.tar
+# MEGA-Release configuration
+CND_PLATFORM_MEGA-Release=AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_DIR_MEGA-Release=dist/MEGA-Release/AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_NAME_MEGA-Release=sonar4
+CND_ARTIFACT_PATH_MEGA-Release=dist/MEGA-Release/AVR-GNU-Toolchain-3.5.3-Linux/sonar4
+CND_PACKAGE_DIR_MEGA-Release=dist/MEGA-Release/AVR-GNU-Toolchain-3.5.3-Linux/package
+CND_PACKAGE_NAME_MEGA-Release=sonar4.tar
+CND_PACKAGE_PATH_MEGA-Release=dist/MEGA-Release/AVR-GNU-Toolchain-3.5.3-Linux/package/sonar4.tar
+# ATtiny84-Release configuration
+CND_PLATFORM_ATtiny84-Release=AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_DIR_ATtiny84-Release=dist/ATtiny84-Release/AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_NAME_ATtiny84-Release=sonar4
+CND_ARTIFACT_PATH_ATtiny84-Release=dist/ATtiny84-Release/AVR-GNU-Toolchain-3.5.3-Linux/sonar4
+CND_PACKAGE_DIR_ATtiny84-Release=dist/ATtiny84-Release/AVR-GNU-Toolchain-3.5.3-Linux/package
+CND_PACKAGE_NAME_ATtiny84-Release=sonar4.tar
+CND_PACKAGE_PATH_ATtiny84-Release=dist/ATtiny84-Release/AVR-GNU-Toolchain-3.5.3-Linux/package/sonar4.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
