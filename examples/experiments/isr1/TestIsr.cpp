ad82645c11f5631d9d847493eac32fdbde421554
Jean-Francois Poilpret <jfpoilpret@gmail.com>
2017-06-10 23:50:39 +0200

    First experiments with I2C.

--- examples/experiments/isr1/TestIsr.cpp
+++ examples/experiments/isr1/TestIsr.cpp
@@ -5,65 +5,157 @@
  */
 
 #include <fastarduino/boards/board.h>
-#include <fastarduino/gpio.h>
 #include <fastarduino/time.h>
-#include <fastarduino/realtime_timer.h>
-#include <fastarduino/utilities.h>
-#include <fastarduino/flash.h>
-#include <fastarduino/devices/hcsr04.h>
-#include <fastarduino/int.h>
-#include <fastarduino/pci.h>
+#include <fastarduino/uart.h>
 
-REGISTER_RTT_ISR(0)
+// UART for traces
+static constexpr const uint8_t OUTPUT_BUFFER_SIZE = 64;
+static char output_buffer[OUTPUT_BUFFER_SIZE];
+static serial::hard::UATX<board::USART::USART0> uart{output_buffer};
+static streams::FormattedOutput<streams::OutputBuffer> out = uart.fout();
+// Define vectors we need in the example
+REGISTER_UATX_ISR(0)
 
-static constexpr const board::DigitalPin TRIGGER = board::DigitalPin::D2_PD2;
-static constexpr const board::DigitalPin ECHO = board::InterruptPin::D3_PD3_PCI2;
-static constexpr const board::Timer TIMER = board::Timer::TIMER0;
-
-using RTT = timer::RTT<TIMER>;
-using SONAR = devices::sonar::HCSR04<TIMER, TRIGGER, ECHO>;
-
-class SonarListener
+// DS1307 specific
+const uint8_t DEVICE_ADDRESS = 0x68 << 1;
+struct BCD
 {
-public:
-	SonarListener(uint16_t min_mm):MIN_US{devices::sonar::distance_mm_to_echo_us(min_mm)}, led_{gpio::PinMode::OUTPUT}
+	struct
 	{
-		interrupt::register_handler(*this);
-	}
-	
-	void on_sonar(uint16_t echo_us)
-	{
-		if (echo_us && echo_us <= MIN_US)
-			led_.set();
-		else
-			led_.clear();
-	}
-	
-private:
-	const uint16_t MIN_US;
-	gpio::FastPinType<board::DigitalPin::LED>::TYPE led_;
+		uint8_t high	:4;
+		uint8_t low		:4;
+	};
+	uint8_t data;
+};
+struct RealTime
+{
+	BCD seconds;
+	BCD minutes;
+	BCD hours;
+	uint8_t weekday;
+	BCD day;
+	BCD month;
+	BCD	year;
 };
 
-REGISTER_HCSR04_PCI_ISR_METHOD(TIMER, 2, TRIGGER, ECHO, SonarListener, &SonarListener::on_sonar)
+//TODO define constants for expected results
 
+const uint32_t I2C_FREQUENCY = 100000;
+
+// NOTE we use prescaler = 1 everywhere
+constexpr uint8_t calculate_TWBR(uint32_t frequency)
+{
+	return (F_CPU / frequency - 16UL) / 2;
+}
+
+static bool wait_twi(uint8_t expected_status)
+{
+	out << "W " << streams::hex << expected_status << " #1" << streams::flush;
+	loop_until_bit_is_set(TWCR, TWINT);
+	out << " #2" << streams::flush;
+	if ((TWSR & 0xF8) == expected_status)
+	{
+		out << "- " << streams::dec << streams::flush;
+		return true;
+	}
+	//TODO ERROR handling and trace
+	out << " X " << streams::hex << TWSR << streams::dec << ' ' << streams::flush;
+	return false;
+}
+
+// TODO traits for: SDA, SCL pins
 int main() __attribute__((OS_main));
 int main()
 {
 	sei();
 	
-	SonarListener listener{150};
+	uart.register_handler();
+	uart.begin(115200);
+	out.width(0);
+	out << "Start\n" << streams::flush;
 	
-	RTT rtt;
-	rtt.register_rtt_handler();
-	rtt.begin();
-	typename interrupt::PCIType<ECHO>::TYPE signal;
-	signal.enable_pin<ECHO>();
-	signal.enable();
-	SONAR sensor{rtt};
+	// Start TWI interface
+	//====================
+	// 1. set SDA/SCL pullups (TODO: in specific TRAIT)
+	PORTC |= _BV(PORTC4) | _BV(PORTC5);
+	// 2. set I2C frequency
+	TWBR = calculate_TWBR(I2C_FREQUENCY);
+	TWSR = 0;
+	// 3. Enable TWI
+	TWCR = _BV(TWEN);
+
+	out << "I2C interface started\n" << streams::flush;
+	time::delay_ms(1000);
 	
-	while (true)
-	{
-		sensor.async_echo();
-		sensor.await_echo_us();
-	}
+	// Send a byte to a slave
+	//=======================
+	// 1. send START
+	TWCR = _BV(TWEN) | _BV(TWINT) | _BV(TWSTA);
+	wait_twi(0x08);
+	// 2. send SLA+W
+	TWDR = DEVICE_ADDRESS;
+	TWCR = _BV(TWEN) | _BV(TWINT);
+	wait_twi(0x18);
+	// 3. send data
+	TWDR = 0;
+	TWCR = _BV(TWEN) | _BV(TWINT);
+	wait_twi(0x28);
+//	// 4. send STOP (normally not needed (or harmful?) with DS1307
+//	TWCR = _BV(TWEN) | _BV(TWINT) | _BV(TWSTO);	
+
+	RealTime time;
+	// Read a byte from a slave
+	//=========================
+	// 1. send REPEATING START
+	TWCR = _BV(TWEN) | _BV(TWINT) | _BV(TWSTA);
+	wait_twi(0x10);
+//	wait_twi(0x08);
+	// 2. send SLA+R
+	TWDR = DEVICE_ADDRESS | 0x01;
+	TWCR = _BV(TWEN) | _BV(TWINT);
+	wait_twi(0x40);
+	// 3. Read data & send ACK
+	TWCR = _BV(TWEN) | _BV(TWINT) | _BV(TWEA);
+	wait_twi(0x50);
+	time.seconds.data = TWDR;
+	TWCR = _BV(TWEN) | _BV(TWINT) | _BV(TWEA);
+	wait_twi(0x50);
+	time.minutes.data = TWDR;
+	TWCR = _BV(TWEN) | _BV(TWINT) | _BV(TWEA);
+	wait_twi(0x50);
+	time.hours.data = TWDR;
+	TWCR = _BV(TWEN) | _BV(TWINT) | _BV(TWEA);
+	wait_twi(0x50);
+	time.weekday = TWDR;
+	TWCR = _BV(TWEN) | _BV(TWINT) | _BV(TWEA);
+	wait_twi(0x50);
+	time.day.data = TWDR;
+	TWCR = _BV(TWEN) | _BV(TWINT) | _BV(TWEA);
+	wait_twi(0x50);
+	time.month.data = TWDR;
+	TWCR = _BV(TWEN) | _BV(TWINT) | _BV(TWEA);
+	wait_twi(0x50);
+	time.year.data = TWDR;
+	TWCR = _BV(TWEN) | _BV(TWINT);
+	wait_twi(0x58);
+	// 4. send STOP
+	TWCR = _BV(TWEN) | _BV(TWINT) | _BV(TWSTO);
+	
+	out	<< "RTC: " 
+		<< time.day.high << time.day.low << '.'
+		<< time.month.high << time.month.low << '.'
+		<< time.year.high << time.year.low << ' '
+		<< time.hours.high << time.hours.low << ':'
+		<< time.minutes.high << time.minutes.low << ':'
+		<< time.seconds.high << time.seconds.low << '\n'
+		<< streams::flush;
+	
+	// Stop TWI interface
+	//===================
+	// 1. Disable TWI
+	TWCR = 0;
+	// 2. remove SDA/SCL pullups
+	PORTC &= ~(_BV(PORTC4) | _BV(PORTC5));
+	
+	out << "End\n" << streams::flush;
 }
