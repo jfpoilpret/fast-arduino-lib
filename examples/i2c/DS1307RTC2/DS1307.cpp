91700e92560a559081bab9c6abf426011ec64c82
Jean-Francois Poilpret <jfpoilpret@gmail.com>
2017-06-14 23:49:55 +0200

    Add I2C support for Arduino LEONARDO.

--- examples/i2c/DS1307RTC2/DS1307.cpp
+++ examples/i2c/DS1307RTC2/DS1307.cpp
@@ -30,6 +30,13 @@ static constexpr const board::USART UART = board::USART::USART0;
 static constexpr const uint8_t OUTPUT_BUFFER_SIZE = 64;
 // Define vectors we need in the example
 REGISTER_UATX_ISR(0)
+#elif defined(ARDUINO_LEONARDO)
+#define HARDWARE_UART 1
+#include <fastarduino/uart.h>
+static constexpr const board::USART UART = board::USART::USART1;
+static constexpr const uint8_t OUTPUT_BUFFER_SIZE = 64;
+// Define vectors we need in the example
+REGISTER_UATX_ISR(1)
 #else
 #error "Current target is not yet supported!"
 #endif
