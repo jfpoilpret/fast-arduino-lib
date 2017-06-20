90847e649c7dbd8df4acee1f31e5cfe7a9b0d93d
Jean-Francois Poilpret <jfpoilpret@gmail.com>
2017-06-20 23:40:20 +0200

    Create specific example dedicated to implementing and debugging new I2C devices.

---
+++ examples/i2c/I2CDeviceProto/I2CDeviceProto.cpp
@@ -0,0 +1,89 @@
+//   Copyright 2016-2017 Jean-Francois Poilpret
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+#include <fastarduino/time.h>
+#include <fastarduino/i2c.h>
+#include <fastarduino/uart.h>
+
+static constexpr const uint8_t OUTPUT_BUFFER_SIZE = 64;
+// Define vectors we need in the example
+REGISTER_UATX_ISR(0)
+
+// UART for traces
+static char output_buffer[OUTPUT_BUFFER_SIZE];
+static serial::hard::UATX<board::USART::USART0> uart{output_buffer};
+static streams::FormattedOutput<streams::OutputBuffer> out = uart.fout();
+
+// Subclass I2CDevice to make protected methods available
+class PublicDevice: public i2c::I2CDevice
+{
+public:
+	PublicDevice(i2c::I2CManager& manager): i2c::I2CDevice{manager} {}
+	friend int main();
+};
+
+// HMC5883L specific
+const uint8_t DEVICE_ADDRESS = 0x1E << 1;
+const uint32_t I2C_FREQUENCY = 400000;
+const uint8_t NUM_REGISTERS = 13;
+
+using streams::flush;
+
+void trace_status(uint8_t expected_status, uint8_t actual_status)
+{
+	out << F("status expected = ") << expected_status << F(", actual = ") << actual_status << '\n' << flush;
+}
+
+void trace_registers(uint8_t registers[NUM_REGISTERS])
+{
+	out << F("HMC5883L registers\n");
+	for (uint8_t i = 0; i < NUM_REGISTERS; ++i)
+		out << F("    ") << i << F(": ") << registers[i] << '\n' << flush;
+	out << flush;
+}
+
+int main() __attribute__((OS_main));
+int main()
+{
+	board::init();
+	sei();
+	
+	uart.register_handler();
+	uart.begin(115200);
+	out.width(2);
+	out.base(streams::FormatBase::Base::hex);
+	out << F("Start\n") << flush;
+	
+	// Start TWI interface
+	//====================
+	i2c::I2CManager manager{trace_status};
+	manager.begin(I2C_FREQUENCY);
+	out << F("I2C interface started\n") << flush;
+	out << F("status #1 ") << manager.status() << '\n' << flush;
+	
+	PublicDevice compass{manager};
+	uint8_t registers[NUM_REGISTERS];
+	
+	// Read device registers
+	//======================
+	compass.write(DEVICE_ADDRESS, uint8_t(0), i2c::BusConditions::START_NO_STOP);
+	compass.read(DEVICE_ADDRESS, registers, i2c::BusConditions::REPEAT_START_STOP);
+	trace_registers(registers);
+
+	// Stop TWI interface
+	//===================
+	manager.end();
+	out << F("status #2 ") << manager.status() << '\n' << flush;
+	out << F("End\n") << flush;
+}
---
+++ examples/i2c/I2CDeviceProto/nbproject/Makefile-variables.mk
@@ -0,0 +1,59 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# UNO-Release configuration
+CND_PLATFORM_UNO-Release=AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_DIR_UNO-Release=dist/UNO-Release/AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_NAME_UNO-Release=ds1307rtc1
+CND_ARTIFACT_PATH_UNO-Release=dist/UNO-Release/AVR-GNU-Toolchain-3.5.3-Linux/ds1307rtc1
+CND_PACKAGE_DIR_UNO-Release=dist/UNO-Release/AVR-GNU-Toolchain-3.5.3-Linux/package
+CND_PACKAGE_NAME_UNO-Release=i2cdeviceproto.tar
+CND_PACKAGE_PATH_UNO-Release=dist/UNO-Release/AVR-GNU-Toolchain-3.5.3-Linux/package/i2cdeviceproto.tar
+# ATmega328-Release configuration
+CND_PLATFORM_ATmega328-Release=AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_DIR_ATmega328-Release=dist/ATmega328-Release/AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_NAME_ATmega328-Release=ds1307rtc1
+CND_ARTIFACT_PATH_ATmega328-Release=dist/ATmega328-Release/AVR-GNU-Toolchain-3.5.3-Linux/ds1307rtc1
+CND_PACKAGE_DIR_ATmega328-Release=dist/ATmega328-Release/AVR-GNU-Toolchain-3.5.3-Linux/package
+CND_PACKAGE_NAME_ATmega328-Release=ds1307rtc1.tar
+CND_PACKAGE_PATH_ATmega328-Release=dist/ATmega328-Release/AVR-GNU-Toolchain-3.5.3-Linux/package/ds1307rtc1.tar
+# MEGA-Release configuration
+CND_PLATFORM_MEGA-Release=AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_DIR_MEGA-Release=dist/MEGA-Release/AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_NAME_MEGA-Release=ds1307rtc1
+CND_ARTIFACT_PATH_MEGA-Release=dist/MEGA-Release/AVR-GNU-Toolchain-3.5.3-Linux/ds1307rtc1
+CND_PACKAGE_DIR_MEGA-Release=dist/MEGA-Release/AVR-GNU-Toolchain-3.5.3-Linux/package
+CND_PACKAGE_NAME_MEGA-Release=ds1307rtc1.tar
+CND_PACKAGE_PATH_MEGA-Release=dist/MEGA-Release/AVR-GNU-Toolchain-3.5.3-Linux/package/ds1307rtc1.tar
+# ATtiny84-Release configuration
+CND_PLATFORM_ATtiny84-Release=AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_DIR_ATtiny84-Release=dist/ATtiny84-Release/AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_NAME_ATtiny84-Release=ds1307rtc1
+CND_ARTIFACT_PATH_ATtiny84-Release=dist/ATtiny84-Release/AVR-GNU-Toolchain-3.5.3-Linux/ds1307rtc1
+CND_PACKAGE_DIR_ATtiny84-Release=dist/ATtiny84-Release/AVR-GNU-Toolchain-3.5.3-Linux/package
+CND_PACKAGE_NAME_ATtiny84-Release=ds1307rtc1.tar
+CND_PACKAGE_PATH_ATtiny84-Release=dist/ATtiny84-Release/AVR-GNU-Toolchain-3.5.3-Linux/package/ds1307rtc1.tar
+# LEONARDO-Release configuration
+CND_PLATFORM_LEONARDO-Release=AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_DIR_LEONARDO-Release=dist/LEONARDO-Release/AVR-GNU-Toolchain-3.5.3-Linux
+CND_ARTIFACT_NAME_LEONARDO-Release=ds1307rtc1
+CND_ARTIFACT_PATH_LEONARDO-Release=dist/LEONARDO-Release/AVR-GNU-Toolchain-3.5.3-Linux/ds1307rtc1
+CND_PACKAGE_DIR_LEONARDO-Release=dist/LEONARDO-Release/AVR-GNU-Toolchain-3.5.3-Linux/package
+CND_PACKAGE_NAME_LEONARDO-Release=ds1307rtc1.tar
+CND_PACKAGE_PATH_LEONARDO-Release=dist/LEONARDO-Release/AVR-GNU-Toolchain-3.5.3-Linux/package/ds1307rtc1.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
