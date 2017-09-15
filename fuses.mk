41631f706b06466bf1763d2c773da299af868e76
jfpoilpret <jfpoilpret@gmail.com>
2017-09-15 21:53:20 +0200

    Temporarily document default fuses for ATmega328 and ATtiny84.

---
+++ fuses.mk
@@ -0,0 +1,29 @@
+# Default fuses configuration for ATmega328 (8MHz)
+# - Internal RC clock 8MHz, startup time PWRDWN/RESET 6CK/14CK + 65ms
+# - Boot reset vector enabled (no bootloader)
+# - Reset enabled
+# - SPI programming enabled
+# - BOD enabled at 2.7V
+# - WDT off
+# Internal RC 8MHz, BOD 2.7V, SPI enabled, no bootloader
+DEF_LFUSE=0xe2
+DEF_HFUSE=0xde
+DEF_EFUSE=0x05
+# For other frequencies, only the following setting changes:
+# - External crystal oscillator (freq 8.0+), startup time PWRDWN/RESET 16CK/14CK +65ms
+DEF_LFUSE=0xff
+
+# Default fuses configuration for ATtiny84 (8MHz)
+# - Internal RC clock 8MHz, startup time PWRDWN/RESET 6CK/14CK + 64ms
+# - Reset enabled
+# - SPI programming enabled
+# - BOD enabled at 2.7V
+# - WDT off
+# Internal RC 8MHz, BOD 2.7V, SPI enabled
+DEF_LFUSE=0xe2
+DEF_HFUSE=0xdd
+DEF_EFUSE=0x01
+# For other frequencies, only the following setting changes:
+# - External crystal oscillator (freq 8.0+), startup time PWRDWN/RESET 16CK/14CK +65ms
+DEF_LFUSE=0xff
+
