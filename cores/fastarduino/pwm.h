92f766fcd123e4d45580eb4c8ea1d5e4b8f718f0
jfpoilpret <jfpoilpret@gmail.com>
2017-09-24 11:35:28 +0200

    Finish PWM API documentation.

--- cores/fastarduino/pwm.h
+++ cores/fastarduino/pwm.h
@@ -37,6 +37,7 @@ namespace analog
 	 * `timer::Timer`; this is useful when e.g. you want to use a PWM pin to
 	 * manage a servo motor, where pulses shall be limited to a few ms but 
 	 * triggered every few dozen ms.
+	 * @sa board::DigitalPin
 	 */
 	template<board::DigitalPin PIN, bool PULSED = false>
 	class PWMOutput
@@ -45,10 +46,24 @@ namespace analog
 		using TIMER_TRAIT = board_traits::Timer_trait<TRAIT::TIMER>;
 		
 	public:
-		//TODO DOC
+		/// @cond notdocumented
 		static constexpr const uint8_t COM = TRAIT::COM;
+		/// @endcond
+
+		/**
+		 * The actual `timer::Timer` type associated to this `PWMOutput`.
+		 */
 		using TIMER = timer::Timer<TRAIT::TIMER>;
 		
+		/**
+		 * Construct a new PWM output pin, connected to @p timer, using 
+		 * @p output_mode.
+		 * This will not compile if `PIN` is not a PWM pin or its is not
+		 * connectable to @p timer.
+		 * @param timer the timer to which this PWM pin must be connected
+		 * @param output_mode the connection output mode to use; note this is not
+		 * used in pulsed mode, i.e. when @p PULSED template argument is `true`.
+		 */
 		PWMOutput(TIMER& timer, TimerOutputMode output_mode = TimerOutputMode::NON_INVERTING):_timer{timer}
 		{
 			static_assert(TRAIT::HAS_PWM, "PIN must be a PWM pin");
@@ -59,15 +74,40 @@ namespace analog
 				_timer.template set_output_mode<COM>(output_mode);
 		}
 		
+		/**
+		 * Change the connection output mode of this PWM pin to its timer.
+		 * @param output_mode the new connection output mode to use; note this 
+		 * is not used in pulsed mode, i.e. when @p PULSED template argument is
+		 * `true`.
+		 */
 		inline void set_output_mode(TimerOutputMode output_mode)
 		{
 			if (TIMER_TRAIT::IS_16BITS || !PULSED)
 				_timer.template set_output_mode<COM>(output_mode);
 		}
 
+		/**
+		 * The type (either `uint8_t` or `uint16_t`) of values acceptable for 
+		 * `duty` in `set_duty()`.
+		 * @sa set_duty()
+		 * @sa MAX
+		 */
 		using TYPE = typename TIMER_TRAIT::TYPE;
+
+		/**
+		 * The maximum acceptable value for `duty` in `set_duty()`.
+		 * @sa set_duty()
+		 * @sa TYPE
+		 */
 		static constexpr const TYPE MAX = TIMER_TRAIT::MAX_PWM;
 		
+		/**
+		 * Set the duty cycle for this PWM pin, from `0` (0% duty cycle) to
+		 * `MAX` (100%), any value above `MAX` will be used as 100%.
+		 * @param duty new duty cycle for this PWM pin
+		 * @sa TYPE
+		 * @sa MAX
+		 */
 		inline void set_duty(TYPE duty)
 		{
 			_timer.template set_max<COM>(duty);
