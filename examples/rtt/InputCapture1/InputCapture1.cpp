a3fb9e0ff3a9d4a02e97134896d0a92eeb6af142
jfpoilpret <jfpoilpret@gmail.com>
2017-10-10 09:35:08 +0200

    Use timer oveflow to compute input capture time.

--- examples/rtt/InputCapture1/InputCapture1.cpp
+++ examples/rtt/InputCapture1/InputCapture1.cpp
@@ -68,13 +68,13 @@ static char output_buffer[OUTPUT_BUFFER_SIZE];
 using TIMER_TYPE = timer::Timer<TIMER>;
 using TYPE = TIMER_TYPE::TIMER_TYPE;
 static constexpr const board::DigitalPin ICP = TIMER_TYPE::ICP_PIN;
-static constexpr const uint32_t PRECISION = 1000UL;
+static constexpr const uint32_t PRECISION = 10000UL;
 using CALC = timer::Calculator<TIMER>;
 static constexpr const TIMER_TYPE::TIMER_PRESCALER PRESCALER = CALC::tick_prescaler(PRECISION);
 static_assert(PRESCALER == TIMER_TYPE::TIMER_PRESCALER::DIV_1024, "PRESCALER should be DIV_1024");
-static constexpr uint16_t milliseconds(uint16_t ticks)
+static constexpr uint32_t milliseconds(uint32_t ticks, uint32_t overflows)
 {
-	return ticks * PRECISION / CALC::CTC_frequency(PRESCALER);
+	return (ticks + overflows * TIMER_TYPE::TIMER_MAX) * 1000UL / CALC::CTC_frequency(PRESCALER);
 }
 
 using timer::TimerInputCapture;
@@ -91,6 +91,7 @@ public:
 		{
 			// Button pushed, prepare for next capture (on button release)
 			_timer._reset();
+			_overflows = 0;
 			_timer.set_input_capture(TimerInputCapture::RISING_EDGE);
 		}
 		else
@@ -102,14 +103,20 @@ public:
 		}
 	}
 
+	void on_overflow()
+	{
+		++_overflows;
+	}
+
 	void start()
 	{
 		synchronized
 		{
 			_ready = false;
 			_capture = 0;
+			_overflows = 0;
 			_timer.set_input_capture(TimerInputCapture::FALLING_EDGE);
-			_timer.set_interrupts(TimerInterrupt::INPUT_CAPTURE);
+			_timer.set_interrupts(TimerInterrupt::INPUT_CAPTURE | TimerInterrupt::OVERFLOW);
 		}
 	}
 
@@ -119,14 +126,21 @@ public:
 		synchronized return _capture;
 	}
 
+	uint16_t overflows()
+	{
+		synchronized return _overflows;
+	}
+
 private:
 	TIMER_TYPE& _timer;
 	gpio::FastPinType<ICP>::TYPE _input;
 	volatile bool _ready;
 	volatile TYPE _capture;
+	volatile uint16_t _overflows;
 };
 
 REGISTER_TIMER_CAPTURE_ISR_METHOD(TIMER_NUM, Capture, &Capture::on_capture)
+REGISTER_TIMER_OVERFLOW_ISR_METHOD(TIMER_NUM, Capture, &Capture::on_overflow)
 
 int main()
 {
@@ -158,7 +172,8 @@ int main()
 		out << F("Waiting for button push...\n") << streams::flush;
 		capture.start();
 		TYPE duration = capture.capture();
-		out << F("Push lasted ") << duration << F(" ticks\n") << streams::flush;
-		out << F("Push lasted ") << milliseconds(duration) << F(" ms\n") << streams::flush;
+		uint16_t overflows = capture.overflows();
+		out << F("Push lasted ") << duration << F(" ticks, ") << overflows << F(" overflows\n") << streams::flush;
+		out << F("Push lasted ") << milliseconds(duration, overflows) << F(" ms\n") << streams::flush;
 	}
 }
