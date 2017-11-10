22e0e31452117daa16a496e26d8e9689b26f70dc
jfpoilpret <jfpoilpret@gmail.com>
2017-11-10 23:04:46 +0100

    Add license header; add gcc references.

--- cores/fastarduino/abi.cpp
+++ cores/fastarduino/abi.cpp
@@ -1,6 +1,20 @@
+//   Copyright 2016-2017 Jean-Francois Poilpret
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 
 #define UNUSED __attribute__((unused))
 
+// https://gcc.gnu.org/onlinedocs/gcc-4.9.2/libstdc++/api/a00833.html
 namespace __cxxabiv1
 {
 	typedef int __guard;
