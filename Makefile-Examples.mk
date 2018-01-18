c232f002c2627860f23e080c69ddda24582c6ec5
jfpoilpret <jfpoilpret@gmail.com>
2018-01-18 22:03:37 +0100

    ATtinyX5: check another RTT example.

--- Makefile-Examples.mk
+++ Makefile-Examples.mk
@@ -84,6 +84,7 @@ EXAMPLES_BREADBOARD_ATTINYX5=	io/FastPin1 io/FastPin2			\
 								analog/AnalogPin1				\
 								events/EventApp6				\
 								rtt/RTTApp1	rtt/RTTApp3			\
+								rtt/RTTApp4						\
 								rtt/TimerApp3 rtt/TimerApp4
 
 # Finally define all examples supported for the current variant (defined by current configuration)
