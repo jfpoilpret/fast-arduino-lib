b72750bfa044b3d5c796395c400cb09afb6a21b0
jfpoilpret <jfpoilpret@gmail.com>
2018-01-19 21:38:49 +0100

    ATtinyX5 support: adapt INT example

--- Makefile-Examples.mk
+++ Makefile-Examples.mk
@@ -85,6 +85,7 @@ EXAMPLES_BREADBOARD_ATTINYX5=	io/FastPin1 io/FastPin2			\
 								analog/PWM1 analog/PWM2			\
 								analog/PWM4						\
 								events/EventApp6				\
+								int/ExternalInterrupt1			\
 								pci/PinChangeInterrupt1			\
 								rtt/RTTApp1	rtt/RTTApp3			\
 								rtt/RTTApp4						\
