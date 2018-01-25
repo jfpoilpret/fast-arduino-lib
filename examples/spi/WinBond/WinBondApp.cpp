598e3ab0825d1f243942e3038ae4bce7a792385b
jfpoilpret <jfpoilpret@gmail.com>
2018-01-25 20:03:16 +0100

    ATtinyX5 support: add SPI Winbond example.

--- examples/spi/WinBond/WinBondApp.cpp
+++ examples/spi/WinBond/WinBondApp.cpp
@@ -18,6 +18,10 @@
  * It checks all WinBond API implemented by WinBond.hh and traces all result to serial.
  * 
  * Wiring:
+ * - WinBond IC:
+ *   - /WP : connect to Vcc
+ *   - /HOLD: connect to Vcc
+ *   - 100nF cap between Vcc and GND
  * - on ATmega328P based boards (including Arduino UNO):
  *   - D1 (TX) used for tracing program activities
  *   - D13 (SCK), D12 (MISO), D11 (MOSI), D7 (CS): SPI interface to WinBond
@@ -26,47 +30,55 @@
  * - on ATtinyX4 based boards:
  *   - D1 (TX) used for tracing program activities
  *   - D4 (SCK), D6 (MISO), D5 (MOSI), D7 (CS): SPI interface to WinBond
+ * - on ATtinyX5 based boards:
+ *   - D4 (TX) used for tracing program activities
+ *   - D0 (MISO), D1 (MOSI), D2 (SCK), D3 (CS): SPI interface to WinBond
  */
 
-#include <util/delay.h>
 #include <fastarduino/devices/winbond.h>
+#include <fastarduino/time.h>
 
 #if defined(ARDUINO_UNO) || defined(BREADBOARD_ATMEGA328P) || defined(ARDUINO_NANO)
 #include <fastarduino/uart.h>
-
 constexpr const board::DigitalPin CS = board::DigitalPin::D7_PD7;
 static const uint8_t OUTPUT_BUFFER_SIZE = 64;
 constexpr const size_t DATA_SIZE = 256;
-
 // Define vectors we need in the example
 constexpr const board::USART UART = board::USART::USART0;
 REGISTER_UATX_ISR(0)
+
 #elif defined(ARDUINO_LEONARDO)
 #include <fastarduino/uart.h>
-
 constexpr const board::DigitalPin CS = board::DigitalPin::D7_PE6;
 static const uint8_t OUTPUT_BUFFER_SIZE = 64;
 constexpr const size_t DATA_SIZE = 256;
-
 // Define vectors we need in the example
 constexpr const board::USART UART = board::USART::USART1;
 REGISTER_UATX_ISR(1)
+
 #elif defined (ARDUINO_MEGA)
 #include <fastarduino/uart.h>
 constexpr const board::DigitalPin CS = board::DigitalPin::D7_PH4;
 static const uint8_t OUTPUT_BUFFER_SIZE = 64;
 constexpr const size_t DATA_SIZE = 256;
-
 // Define vectors we need in the example
 constexpr const board::USART UART = board::USART::USART0;
 REGISTER_UATX_ISR(0)
+
 #elif defined (BREADBOARD_ATTINYX4)
 #include <fastarduino/soft_uart.h>
-
 constexpr const board::DigitalPin TX = board::DigitalPin::D1_PA1;
 constexpr const board::DigitalPin CS = board::DigitalPin::D7_PA7;
 static const uint8_t OUTPUT_BUFFER_SIZE = 64;
 constexpr const size_t DATA_SIZE = 128;
+
+#elif defined (BREADBOARD_ATTINYX5)
+#include <fastarduino/soft_uart.h>
+constexpr const board::DigitalPin CS = board::DigitalPin::D3_PB3;
+constexpr const board::DigitalPin TX = board::DigitalPin::D4_PB4;
+static const uint8_t OUTPUT_BUFFER_SIZE = 64;
+constexpr const size_t DATA_SIZE = 128;
+
 #else
 #error "Current target is not yet supported!"
 #endif
@@ -78,6 +90,8 @@ static uint8_t data[DATA_SIZE];
 
 constexpr const uint32_t PAGE = 0x010000;
 
+using namespace streams;
+
 int main()
 {
 	board::init();
@@ -85,81 +99,80 @@ int main()
 	sei();
 
 	// Start UART
-#if defined (BREADBOARD_ATTINYX4)
+#if defined (BREADBOARD_ATTINYX4) || defined (BREADBOARD_ATTINYX5)
 	serial::soft::UATX<TX> uart{output_buffer};
-	uart.begin(115200);
 #else
 	serial::hard::UATX<UART> uart{output_buffer};
 	uart.register_handler();
-	uart.begin(115200);
 #endif
-	streams::ostream out = uart.out();
+	uart.begin(115200);
+	ostream out = uart.out();
 	
-	out << "Started\n";
+	out << F("Started") << endl;
 
 	spi::init();
 	devices::WinBond<CS> flash;
-	_delay_ms(1000);
+	time::delay_ms(1000);
 	
-	out << "S: " << streams::hex << flash.status().value << streams::endl;
+	out << F("S: ") << hex << flash.status().value << endl;
 	uint64_t id = flash.read_unique_ID();
-	out << "UID: " << streams::hex << uint16_t(id >> 48) << ' ' << uint16_t(id >> 32) << ' ' 
-					<< uint16_t(id >> 16) << ' ' << uint16_t(id) << streams::endl;
+	out << F("UID: ") << hex << uint16_t(id >> 48) << ' ' << uint16_t(id >> 32) << ' ' 
+					<< uint16_t(id >> 16) << ' ' << uint16_t(id) << endl;
 	devices::WinBond<CS>::Device device = flash.read_device();
-	out << "M ID: " << streams::hex << device.manufacturer_ID << streams::endl;
-	out << "D ID: " << streams::hex << device.device_ID << streams::endl;
+	out << F("M ID: ") << hex << device.manufacturer_ID << endl;
+	out << F("D ID: ") << hex << device.device_ID << endl;
 
-	out << "B4 RD 1 pg, S: " << streams::hex << flash.status().value << streams::endl;
+	out << F("B4 RD 1 pg, S: ") << hex << flash.status().value << endl;
 	flash.read_data(PAGE, data, sizeof data);
-	out << "Af RD, S: " << streams::hex << flash.status().value << streams::endl;
+	out << F("Af RD, S: ") << hex << flash.status().value << endl;
 
-	out << "Pg RD:" << streams::endl;
+	out << F("Pg RD:") << endl;
 	for (uint16_t i = 0; i < sizeof data; ++i)
 	{
-		out << streams::hex << data[i] << ' ';
+		out << hex << data[i] << ' ';
 		if ((i + 1) % 16 == 0)
-			out << streams::endl;
+			out << endl;
 	}
-	out << streams::endl;
+	out << endl;
 	
-	out << "B4 erase, S: " << streams::hex << flash.status().value << streams::endl;
+	out << F("B4 erase, S: ") << hex << flash.status().value << endl;
 	flash.enable_write();
-	out << "Af enable WR, S: " << streams::hex << flash.status().value << streams::endl;
+	out << F("Af enable WR, S: ") << hex << flash.status().value << endl;
 	flash.erase_sector(PAGE);
-	out << "Af erase, S: " << streams::hex << flash.status().value << streams::endl;
+	out << F("Af erase, S: ") << hex << flash.status().value << endl;
 
 	flash.wait_until_ready(10);
-	out << "Af wait, S: " << streams::hex << flash.status().value << streams::endl;
+	out << F("Af wait, S: ") << hex << flash.status().value << endl;
 
 	for (uint16_t i = 0; i < sizeof data; ++i)
 		data[i] = uint8_t(i);
 
-	out << "B4 WR, S: " << streams::hex << flash.status().value << streams::endl;
+	out << F("B4 WR, S: ") << hex << flash.status().value << endl;
 	flash.enable_write();
 	flash.write_page(PAGE, data, sizeof data);
-	out << "Af WR, S: " << streams::hex << flash.status().value << streams::endl;
+	out << F("Af WR, S: ") << hex << flash.status().value << endl;
 	
 	flash.wait_until_ready(10);
-	out << "Af wait, S: " << streams::hex << flash.status().value << streams::endl;
+	out << F("Af wait, S: ") << hex << flash.status().value << endl;
 	
 	for (uint16_t i = 0; i < sizeof data; ++i)
 		data[i] = 0;
 	
-	out << "B4 RD 1 byte, S: " << streams::hex << flash.status().value << streams::endl;
+	out << F("B4 RD 1 byte, S: ") << hex << flash.status().value << endl;
 	uint8_t value = flash.read_data(PAGE + sizeof(data) / 2);
-	out << "RD " << value << ", S: " << streams::hex << flash.status().value << streams::endl;
+	out << F("RD ") << value << F(", S: ") << hex << flash.status().value << endl;
 	
-	out << "B4 RD 1 pg, S: " << streams::hex << flash.status().value << streams::endl;
+	out << F("B4 RD 1 pg, S: ") << hex << flash.status().value << endl;
 	flash.read_data(PAGE, data, sizeof data);
-	out << "Af RD, S: " << streams::hex << flash.status().value << streams::endl;
+	out << F("Af RD, S: ") << hex << flash.status().value << endl;
 
-	out << "Pg RD:" << streams::endl;
+	out << F("Pg RD:") << endl;
 	for (uint16_t i = 0; i < sizeof data; ++i)
 	{
-		out << streams::hex << data[i] << ' ';
+		out << hex << data[i] << ' ';
 		if ((i + 1) % 16 == 0)
-			out << streams::endl;
+			out << endl;
 	}
 
-	out << "\nFinished" << streams::endl;
+	out << F("\nFinished") << endl;
 }
