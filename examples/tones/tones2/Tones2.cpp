0811bf0967ee03b22544081ac3ac1be9b2e59e8e
jfpoilpret <jfpoilpret@gmail.com>
2018-02-04 15:36:20 +0100

    Update Tone2 & Tone3 examples to use optimized QTonePlay.

--- examples/tones/tones2/Tones2.cpp
+++ examples/tones/tones2/Tones2.cpp
@@ -16,70 +16,71 @@ static constexpr const board::Timer NTIMER = board::Timer::TIMER1;
 static constexpr const board::DigitalPin OUTPUT = board::PWMPin::D9_PB1_OC1A;
 
 using devices::audio::Tone;
+using namespace devices::audio::SpecialTone;
 using GENERATOR = devices::audio::ToneGenerator<NTIMER, OUTPUT>;
 using PLAYER = devices::audio::TonePlayer<NTIMER, OUTPUT>;
+using QTONEPLAY = PLAYER::QTonePlay;
 
-using devices::audio::TonePlay;
-
-const TonePlay music[] EEMEM =
+static QTONEPLAY music[] EEMEM =
 {
 	// First part
-	{Tone::A1, 500},
-	{Tone::A1, 500},
-	{Tone::A1, 500},
-	{Tone::F1, 350},
-	{Tone::C2, 150},
-	{Tone::A1, 500},
-	{Tone::F1, 350},
-	{Tone::C2, 150},
-	{Tone::A1, 650},
-	{Tone::NONE, 150},
+	QTONEPLAY{Tone::A1, 500},
+	QTONEPLAY{Tone::A1, 500},
+	QTONEPLAY{Tone::A1, 500},
+	QTONEPLAY{Tone::F1, 350},
+	QTONEPLAY{Tone::C2, 150},
+	QTONEPLAY{Tone::A1, 500},
+	QTONEPLAY{Tone::F1, 350},
+	QTONEPLAY{Tone::C2, 150},
+	QTONEPLAY{Tone::A1, 650},
+	QTONEPLAY{Tone::SILENCE, 150},
 
 	// Second part
-	{Tone::E2, 500},
-	{Tone::E2, 500},
-	{Tone::E2, 500},
-	{Tone::F2, 350},
-	{Tone::C2, 150},
-	{Tone::Gs1, 500},
-	{Tone::F1, 350},
-	{Tone::C2, 150},
-	{Tone::A1, 650},
-	{Tone::NONE, 150},
+	QTONEPLAY{Tone::E2, 500},
+	QTONEPLAY{Tone::E2, 500},
+	QTONEPLAY{Tone::E2, 500},
+	QTONEPLAY{Tone::F2, 350},
+	QTONEPLAY{Tone::C2, 150},
+	QTONEPLAY{Tone::Gs1, 500},
+	QTONEPLAY{Tone::F1, 350},
+	QTONEPLAY{Tone::C2, 150},
+	QTONEPLAY{Tone::A1, 650},
+	QTONEPLAY{Tone::SILENCE, 150},
 
 	// Third part (repeated once)
-	{Tone::REPEAT_START, 0},
-	{Tone::A2, 500},
-	{Tone::A1, 300},
-	{Tone::A1, 150},
-	{Tone::A2, 400},
-	{Tone::Gs2, 200},	//too short
-	{Tone::G2, 200},
-	{Tone::Fs2, 125},
-	{Tone::F2, 125},
-	{Tone::Fs2, 250},
-	{Tone::NONE, 250},
+	QTONEPLAY{REPEAT_START},
+	QTONEPLAY{Tone::A2, 500},
+	QTONEPLAY{Tone::A1, 300},
+	QTONEPLAY{Tone::A1, 150},
+	QTONEPLAY{Tone::A2, 400},
+	QTONEPLAY{Tone::Gs2, 200},
+	QTONEPLAY{Tone::G2, 200},
+	QTONEPLAY{Tone::Fs2, 125},
+	QTONEPLAY{Tone::F2, 125},
+	QTONEPLAY{Tone::Fs2, 250},
+	QTONEPLAY{Tone::SILENCE, 250},
 
-	{Tone::As1, 250},
-	{Tone::Ds2, 400},
-	{Tone::D2, 200},
-	{Tone::Cs2, 200},	// this frequency produces "bad buzz" :-)
-	{Tone::C2, 125},
-	{Tone::B1, 125},
-	{Tone::C2, 250},
-	{Tone::NONE, 250},
+	QTONEPLAY{Tone::As1, 250},
+	QTONEPLAY{Tone::Ds2, 400},
+	QTONEPLAY{Tone::D2, 200},
+	QTONEPLAY{Tone::Cs2, 200},
+	QTONEPLAY{Tone::C2, 125},
+	QTONEPLAY{Tone::B1, 125},
+	QTONEPLAY{Tone::C2, 250},
+	QTONEPLAY{Tone::SILENCE, 250},
 
-	{Tone::F1, 125},
-	{Tone::Gs1, 500},
-	{Tone::F1, 375},
-	{Tone::A1, 125},
-	{Tone::C2, 500},
-	{Tone::A1, 375},
-	{Tone::C2, 125},
-	{Tone::E2, 650},
-	{Tone::NONE, 250},
-	{Tone::REPEAT_END, 1},
-	{Tone::END, 0}
+	QTONEPLAY{Tone::F1, 125},
+	QTONEPLAY{Tone::Gs1, 500},
+	QTONEPLAY{Tone::F1, 375},
+	QTONEPLAY{Tone::A1, 125},
+	QTONEPLAY{Tone::C2, 500},
+	QTONEPLAY{Tone::A1, 375},
+	QTONEPLAY{Tone::C2, 125},
+	QTONEPLAY{Tone::E2, 650},
+	QTONEPLAY{Tone::SILENCE, 250},
+	QTONEPLAY{REPEAT_END, 1},
+
+	QTONEPLAY{END, 0}
 };
 
 int main() __attribute__((OS_main));
