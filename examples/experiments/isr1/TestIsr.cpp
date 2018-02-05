7129c698de3969ddcc876695632bc63506cbbc61
jfpoilpret <jfpoilpret@gmail.com>
2018-02-05 22:32:22 +0100

    Create snippet writing to EEPROM for tutorial.

--- examples/experiments/isr1/TestIsr.cpp
+++ examples/experiments/isr1/TestIsr.cpp
@@ -4,54 +4,76 @@
  * It does not do anything interesting as far as hardware is concerned.
  */
 
-// Imperial march tones thanks:
-// http://processors.wiki.ti.com/index.php/Playing_The_Imperial_March
+#include <string.h>
+#include <fastarduino/eeprom.h>
+#include <fastarduino/uart.h>
 
-// Example of square wave generation, using CTC mode and COM toggle
-#include <fastarduino/time.h>
-#include <fastarduino/devices/tone_player.h>
+// Define vectors we need in the example
+REGISTER_UART_ISR(0)
+static const board::USART USART = board::USART::USART0;
 
-// Board-dependent settings
-// static constexpr const board::Timer NTIMER = board::Timer::TIMER1;
-// static constexpr const board::DigitalPin OUTPUT = board::PWMPin::D9_PB1_OC1A;
-static constexpr const board::Timer NTIMER = board::Timer::TIMER0;
-static constexpr const board::DigitalPin OUTPUT = board::PWMPin::D6_PD6_OC0A;
+// Buffers for UART
+static const uint8_t INPUT_BUFFER_SIZE = 64;
+static const uint8_t OUTPUT_BUFFER_SIZE = 64;
+static char input_buffer[INPUT_BUFFER_SIZE];
+static char output_buffer[OUTPUT_BUFFER_SIZE];
 
-using devices::audio::Tone;
-using namespace devices::audio::SpecialTone;
-using GENERATOR = devices::audio::ToneGenerator<NTIMER, OUTPUT>;
-using PLAYER = devices::audio::TonePlayer<NTIMER, OUTPUT>;
+static const uint8_t MAX_LEN = 64;
+char wifi_name[MAX_LEN+1] EEMEM = "";
+char wifi_password[MAX_LEN+1] EEMEM = "";
 
-using QTONEPLAY = PLAYER::QTonePlay;
+using namespace eeprom;
+using streams::endl;
+using streams::flush;
+using streams::noskipws;
+using streams::skipws;
 
-const QTONEPLAY music[] EEMEM =
-// const QTONEPLAY music[] PROGMEM =
-// const QTONEPLAY music[] =
-{
-	// Intro
-	QTONEPLAY{Tone::A1, 500},
-	QTONEPLAY{Tone::A1, 500},
-	QTONEPLAY{Tone::A1, 500},
-	QTONEPLAY{Tone::F1, 350},
-	QTONEPLAY{Tone::C2, 150},
-	QTONEPLAY{Tone::A1, 500},
-	QTONEPLAY{Tone::F1, 350},
-	QTONEPLAY{Tone::C2, 150},
-	QTONEPLAY{Tone::A1, 650},
-
-	// Marker for end of melody
-	QTONEPLAY{END}
-};
-
-int main() __attribute__((OS_main));
+// int main() __attribute__((OS_main));
 int main()
 {
+	board::init();
+	// Enable interrupts at startup time
 	sei();
-	time::delay_ms(5000);
+	
+	// Start UART
+	serial::hard::UART<USART> uart{input_buffer, output_buffer};
+	uart.register_handler();
+	uart.begin(115200);
 
-	GENERATOR generator;
-	PLAYER player{generator};
-	player.play_eeprom(music);
-	// player.play_flash(music);
-	// player.play(music);
+	streams::istream in = uart.in();
+	streams::ostream out = uart.out();
+
+	// Get current WIFI name/password from EEPROM
+	char wifi[MAX_LEN+1];
+	EEPROM::read(wifi_name, wifi, MAX_LEN+1);
+	char password[MAX_LEN+1];
+	EEPROM::read(wifi_password, password, MAX_LEN+1);
+
+	// If WIFI present check if user wants to keep it
+	bool ask = true;
+	if (strlen(wifi))
+	{
+		char answer;
+		out << F("Do you want to use WIFI `") << wifi << F("`? [Y/n] :") << flush;
+		in >> skipws >> answer;
+		in.ignore(0, '\n');
+		ask = (toupper(answer) == 'N');
+	}
+
+	// Ask for WIKI name & password if needed
+	if (ask)
+	{
+		out << F("Enter WIFI name: ") << flush;
+		in.getline(wifi, MAX_LEN+1);
+		out << F("Enter WIFI password: ") << flush;
+		in.getline(password, MAX_LEN+1);
+		// Store to EEPROM
+		EEPROM::write(wifi_name, wifi, strlen(wifi) + 1);
+		EEPROM::write(wifi_password, password, strlen(password) + 1);
+	}
+
+	// Start real program here, using wifi and password
+	//...
+	out << F("WIFI: ") << wifi << endl;
+	out << F("Password: ") << password << endl;
 }
