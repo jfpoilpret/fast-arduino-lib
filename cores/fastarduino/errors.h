af61afc69e555ba77b1b57f699729e2b1a8db7be
jfpoilpret <jfpoilpret@gmail.com>
2018-02-08 21:48:53 +0100

    Document errors.h

--- cores/fastarduino/errors.h
+++ cores/fastarduino/errors.h
@@ -12,16 +12,36 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
+/// @cond api
+
+/**
+ * @file 
+ * Common errors definition.
+ */
 #ifndef ERRORS_HH
 #define ERRORS_HH
 
-// List of errors returned by some FastArduino methods
+/**
+ * This namespace defines common errors that can be returned by some FastArduino
+ * API methods, e.g. devices::rf::NRF24L01.
+ * New errors will be added here as required.
+ * These definitions are similar to standard C `errno.h`; however, please note
+ * that FastArduino does not define a global `errno`, as in standard C, to
+ * store the last occurred error. The error constants defined here are directly 
+ * returned, as `int`, by some methods in FastArduino API.
+ * Also, only error codes potentially returned by FastArduino API are defined here.
+ */
 namespace errors
 {
+	/** Input/output error. */
 	constexpr const int EIO = -5;
+	/** Invalid argument. */
 	constexpr const int EINVAL = -22;
+	/** Timer expired. */
 	constexpr const int ETIME = -62;
+	/** Message too long.  */
 	constexpr const int EMSGSIZE = -90;
 }
 
 #endif /* ERRORS_HH */
+/// @endcond
