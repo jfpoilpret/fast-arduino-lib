b5c8a87a6f37a56baa762fe0c1767954b62bcc91
jfpoilpret <jfpoilpret@gmail.com>
2019-04-21 15:29:02 +0200

    Improve MCP23017 example to test inputs.

--- examples/i2c/MultiIO1/MultiIO1.cpp
+++ examples/i2c/MultiIO1/MultiIO1.cpp
@@ -16,16 +16,46 @@
 #include <fastarduino/i2c_device.h>
 #include <fastarduino/devices/mcp23017.h>
 
-// constexpr const i2c::I2CMode I2C_MODE = i2c::I2CMode::Standard;
 constexpr const i2c::I2CMode I2C_MODE = i2c::I2CMode::Fast;
 
 using MCP = devices::MCP23017<I2C_MODE>;
 using MCP_PORT = devices::MCP23017Port;
 
-static inline uint8_t shift_pattern(uint8_t pattern, uint8_t shift)
+static inline uint8_t shift_pattern(uint8_t pattern, uint8_t shift, bool direction)
 {
+	if (direction) shift = 8 - shift;
 	uint16_t result = (pattern << shift);
 	return result | (result >> 8);
+}
+
+static inline uint8_t calculate_pattern(uint8_t switches)
+{
+	switch ((~switches) & 0x07)
+	{
+		case 0x00:
+		return 0x01;
+
+		case 0x01:
+		return 0x03;
+
+		case 0x02:
+		return 0x07;
+
+		case 0x03:
+		return 0x0F;
+
+		case 0x04:
+		return 0x55;
+
+		case 0x05:
+		return 0x33;
+
+		case 0x06:
+		return 0x11;
+
+		case 0x07:
+		return 0xDB;
+	}
 }
 
 int main() __attribute__((OS_main));
@@ -44,17 +74,27 @@ int main()
 	time::delay_ms(100);
 	MCP mcp{manager, 0x00};
 	mcp.configure_gpio<MCP_PORT::PORT_A>(0x00);
-	// mcp.configure_gpio(SWITCH_MASK, SWITCH_MASK);
+	mcp.configure_gpio<MCP_PORT::PORT_B>(0x0F, 0x0F);
 
 	mcp.values<MCP_PORT::PORT_A>(0x11);
 	time::delay_ms(1000);
 	mcp.values<MCP_PORT::PORT_A>(0x00);
 	// Loop of the LED chaser
+	uint8_t switches = mcp.values<MCP_PORT::PORT_B>() & 0x0F;
+	bool direction = (~switches) & 0x08;
+	uint8_t pattern = calculate_pattern(switches);
 	while (true)
 	{
+		uint8_t new_switches = mcp.values<MCP_PORT::PORT_B>() & 0x0F;
+		if (switches != new_switches)
+		{
+			switches = new_switches;
+			direction = (~switches) & 0x08;
+			pattern = calculate_pattern(switches);
+		}
 		for (uint8_t i = 0; i < 8; ++i)
 		{
-			mcp.values<MCP_PORT::PORT_A>(shift_pattern(0x01, i));
+			mcp.values<MCP_PORT::PORT_A>(shift_pattern(pattern, i, direction));
 			time::delay_ms(250);
 		}
 	}
