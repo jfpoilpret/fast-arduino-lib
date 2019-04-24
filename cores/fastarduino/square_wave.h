a84ab92439ae05c858a3b6b34892ae73495a5ae8
jfpoilpret <jfpoilpret@gmail.com>
2019-04-25 00:04:40 +0200

    Add API doc.

--- cores/fastarduino/square_wave.h
+++ cores/fastarduino/square_wave.h
@@ -12,6 +12,12 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
+/// @cond api
+
+/**
+ * @file 
+ * SquareWave API, an API to generate square waves of any frequency.
+ */
 #ifndef SQUARE_WAVE_HH
 #define SQUARE_WAVE_HH
 
@@ -20,16 +26,42 @@
 
 namespace timer
 {
+	/**
+	 * Simple API to generate a square wave to an output pin.
+	 * This uses a Timer and the Ouput pin OCxA of that Timer.
+	 * 
+	 * This class can be useful in many situations where you need to generate
+	 * a square wave of a given frequency (or several frequencies) on an output
+	 * pin. For example, it can be used to produce "music" with a buzzer.
+	 * This is what `devices::audio::ToneGenerator` and `devices::audio::TonePlayer`
+	 * do.
+	 * 
+	 * @tparam NTIMER_ the AVR timer to use for the underlying Timer
+	 * @tparam OUTPUT_ the DigitalPin connected to this SquareWave generator;
+	 * this must be the pin OCnA, where n is the AVR Timer number
+	 * 
+	 * @sa devices::audio::ToneGenerator
+	 * @sa devices::audio::TonePlayer
+	 */
 	template<board::Timer NTIMER_, board::DigitalPin OUTPUT_> class SquareWave
 	{
 	public:
+		/** The AVR timer used for the underlying Timer. */
 		static constexpr const board::Timer NTIMER = NTIMER_;
+		/** The board::DigitalPin connected to this SquareWave generator. */
 		static constexpr const board::DigitalPin OUTPUT = OUTPUT_;
 
+		/** The timer::Calculator type for the underlying timer. */
 		using CALC = timer::Calculator<NTIMER>;
+		/** The type of underlying timer::Timer used by this SquareWave generator. */
 		using TIMER = timer::Timer<NTIMER>;
+		/** The analog::PWMOutput type for the @p OUTPUT board::DigitalPin. */
 		using PWMPIN = analog::PWMOutput<OUTPUT>;
 
+		/**
+		 * Instantiate a SquareWave generator.
+		 * This will create the underlying Timer with the proper arguments.
+		 */
 		SquareWave()
 			: timer_{timer::TimerMode::CTC, TIMER::PRESCALER::NO_PRESCALING},
 			  output_{timer_, timer::TimerOutputMode::TOGGLE}
@@ -38,11 +70,28 @@ namespace timer
 			static_assert(TRAIT::COM == 0, "Only OCnA pin is supported for wave generation");
 		}
 
+		//FIXME I doubt this method can really be "const": why doesn't the compiler complain?
+		/**
+		 * Return the underlying timer::Timer of this SquareWave generator.
+		 */
 		TIMER& timer() const
 		{
 			return timer_;
 		}
 
+		/**
+		 * Start producing, on the ouput pin, a square wave with the specified
+		 * frequency.
+		 * 
+		 * This method performs heavy calculation to set the proper attributes for
+		 * the underlying Timer.
+		 * If possible, you should prefer the other start_frequency() that directly
+		 * takes a prescaler and a counter value (those can be pre-calculated at
+		 * compile-time).
+		 * @param the frequency, in Hz, of the square wave t generate
+		 * 
+		 * @sa start_frequency(typename TIMER::PRESCALER, typename TIMER::TYPE)
+		 */
 		void start_frequency(uint32_t frequency)
 		{
 			const uint32_t period = 1000000UL / 2 / frequency;
@@ -50,6 +99,23 @@ namespace timer
 			start_frequency(prescaler, CALC::CTC_counter(prescaler, period));
 		}
 
+		/**
+		 * Start producing, on the output pin, a square wave, which frequency
+		 * matches the specified @p prescaler and @p counter arguments.
+		 * This method performs no heavy calculation and should be preferred
+		 * over `start_frequency(uint32_t)` if the used frequency is known at
+		 * compile-time.
+		 * @param prescaler the prescaler to use on the underlying Timer, in 
+		 * order to produce the desired frequency. It can be calculated by
+		 * `CALC::CTC_prescaler()`
+		 * @param counter the counter value to use on the underlying Timer, in
+		 * order to produce the desired frequency. It can be calculated by
+		 * `CALC::CTC_counter()`
+		 * 
+		 * @sa start_frequency(uint32_t)
+		 * @sa timer::Calculator<NTIMER_>::CTC_prescaler()
+		 * @sa timer::Calculator<NTIMER_>::CTC_counter()
+		 */
 		void start_frequency(typename TIMER::PRESCALER prescaler, typename TIMER::TYPE counter)
 		{
 			timer_.end();
@@ -58,6 +124,9 @@ namespace timer
 			output_.set_duty(counter);
 		}
 
+		/**
+		 * Stop square wave generation.
+		 */
 		void stop()
 		{
 			timer_.end();
@@ -71,3 +140,4 @@ namespace timer
 }
 
 #endif /* SQUARE_WAVE_HH */
+/// @endcod
