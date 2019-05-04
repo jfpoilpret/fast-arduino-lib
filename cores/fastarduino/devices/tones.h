95188cf7dad70bf3caa8a6d82b7802507593dadd
jfpoilpret <jfpoilpret@gmail.com>
2019-05-04 23:46:06 +0200

    Add API doc for ToneGenerator.

--- cores/fastarduino/devices/tones.h
+++ cores/fastarduino/devices/tones.h
@@ -12,14 +12,37 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
+/// @cond api
+
+/**
+ * @file
+ * API to handle tones (simple square waves) generation to a buzzer.
+ */
 #ifndef TONES_HH
 #define TONES_HH
 
 #include "../square_wave.h"
 #include "../time.h"
 
+/**
+ * Defines API for audio tones (square waves) generation.
+ */
 namespace devices::audio
 {
+	/**
+	 * This enum defines all possible audio tones that can be generated.
+	 * This also defnes "special" values that are not actual tones but are 
+	 * reserved for user purposes or for silences:
+	 * - `USER0` ... `USER7`: can be used for any purpose defined by the end-developer
+	 * - `SILENCE`: used to play no tone at all
+	 * All other tones are named according to their musical note (in English scale),
+	 * and their octave,. This is similar to the usual
+	 * [Scientific Pitch Notation](https://en.wikipedia.org/wiki/Scientific_pitch_notation)
+	 * except for the octave index which is different: in this enum, the standard
+	 * tuning pitch (440Hz) is `A1` instead of `A4` in *SPN*.
+	 * Please note that each tone can be converted to a `uint16_t` which is its
+	 * playing frequency.
+	 */
 	enum class Tone : uint16_t
 	{
 		USER0 = 0,
@@ -30,15 +53,6 @@ namespace devices::audio
 		USER5,
 		USER6,
 		USER7,
-		// // Special marker for a tone sequence, meaning there is no more tone in the sequence
-		// // This is useful when you don't know the sequence size in advance
-		// END = 0,
-
-		// // Use this "tone" to mark the beginning of a sequence that shall be repeated when REPEAT_END is encountered
-		// REPEAT_START = 1,
-		// // Use this "tone" to mark the end of a sequence to repeat from REPEAT_START
-		// // In TonePlay, ms then contains the number of times to repeat the sequence
-		// REPEAT_END = 2,
 
 		// Use this tone for pause (no tone)
 		SILENCE = USER7 + 1,
@@ -109,6 +123,26 @@ namespace devices::audio
 		B4 = 3951,
 	};
 
+	/**
+	 * API class for tone generation to a buzzer (or better an amplifier) connected
+	 * to pin @p OUTPUT.
+	 * This is a rather low-level API. If you want to play suites of tones ("melodies"),
+	 * then you should better use `devices::audio::TonePlayer`.
+	 * Most provided methods come in two flavours:
+	 * - one that takes a `Tone` argument (that will be converted to a note frequency)
+	 * - one that takes a TimerPrescaler and a counter value, that will be used to
+	 * generate the note frequency from raw timer values.
+	 * The first flavour is more readable but less efficient (code size and performance).
+	 * The second flavour allows for performance optimization (compile-time calculations)
+	 * but is less easy to read and understand.
+	 * 
+	 * @tparam NTIMER the AVR timer to use for the underlying Timer
+	 * @tparam OUTPUT the DigitalPin connected to the buzzer;
+	 * this must be the pin OCnA, where n is the AVR Timer number
+	 * 
+	 * @sa Tone
+	 * @sa TonePlayer
+	 */
 	template<board::Timer NTIMER, board::DigitalPin OUTPUT> class ToneGenerator
 	{
 	private:
@@ -116,34 +150,64 @@ namespace devices::audio
 		static constexpr const uint32_t INTERTONE_DELAY_MS = 20;
 
 	public:
+		/** The `TimerPrescaler` type matching the selected NTIMER. */
 		using PRESCALER = typename SQWGEN::TIMER::PRESCALER;
+		/** The counter type (`uint8_t` or `uint16_t`) for the selected NTIMER. */
 		using COUNTER = typename SQWGEN::TIMER::TYPE;
 
+		/**
+		 * Create an ew generator of tones.
+		 */
 		ToneGenerator() : generator_{} {}
 
+		/**
+		 * Start generating a tone on the connected buzzer until `stop_tone()`
+		 * or `pause()` is called.
+		 * If you would like to generate a tone for a given duration, you should
+		 * use `tone()` instead.
+		 * 
+		 * @param t the tone to generate
+		 * 
+		 * @sa stop_tone()
+		 * @sa pause()
+		 * @sa tone(Tone, uint16_t)
+		 * @sa start_tone(PRESCALER, COUNTER)
+		 */
 		void start_tone(Tone t)
 		{
 			if (t > Tone::SILENCE) generator_.start_frequency(uint32_t(t));
 		}
+
+		/**
+		 * Start generating a tone on the connected buzzer until `stop_tone()`
+		 * or `pause()` is called.
+		 * If you would like to generate a tone for a given duration, you should
+		 * use `tone()` instead.
+		 * 
+		 * @param prescaler the timer prescaler value to use to produce the required tone
+		 * @param counter the timer counter value to use to produce the required tone
+		 * 
+		 * @sa stop_tone()
+		 * @sa pause()
+		 * @sa tone(PRESCALER, COUNTER, uint16_t)
+		 * @sa start_tone(Tone)
+		 */
 		inline void start_tone(PRESCALER prescaler, COUNTER counter)
 		{
 			generator_.start_frequency(prescaler, counter);
 		}
-		inline void stop_tone()
-		{
-			generator_.stop();
-		}
 
-		void tone(Tone t, uint16_t ms)
-		{
-			if (t > Tone::SILENCE) generator_.start_frequency(uint32_t(t));
-			if (t >= Tone::SILENCE) pause(ms);
-		}
-		inline void tone(PRESCALER prescaler, COUNTER counter, uint16_t ms)
-		{
-			generator_.start_frequency(prescaler, counter);
-			pause(ms);
-		}
+		/**
+		 * Force a delay of the required duration and stop the tone being currently
+		 * generated to the connected buzzer.
+		 * Note that, after the note generation has been stopped, a short silence
+		 * of 20ms is forced.
+		 * 
+		 * @param ms the time to wait, in milliseconds, until note generation is
+		 * stopped
+		 * 
+		 * @sa start_tone()
+		 */
 		inline void pause(uint16_t ms)
 		{
 			time::delay_ms(ms);
@@ -152,9 +216,50 @@ namespace devices::audio
 			time::delay_ms(INTERTONE_DELAY_MS);
 		}
 
+		/**
+		 * Stop the tone being currently generated to the connected buzzer.
+		 * @sa start_toner()
+		 */
+		inline void stop_tone()
+		{
+			generator_.stop();
+		}
+
+		/**
+		 * Play the required tone for the given duration.
+		 * Note that at the end of the tone, a short silence of 20ms is forced.
+		 * 
+		 * @param t the tone to play
+		 * @param ms the duration, in milliseconds, of the tone to generate
+		 * 
+		 * @start_tone(Tone)
+		 */
+		void tone(Tone t, uint16_t ms)
+		{
+			if (t > Tone::SILENCE) generator_.start_frequency(uint32_t(t));
+			if (t >= Tone::SILENCE) pause(ms);
+		}
+		
+		/**
+		 * Play the required tone for the given duration.
+		 * Note that at the end of the tone, a short silence of 20ms is forced.
+		 * 
+		 * @param prescaler the timer prescaler value to use to produce the required tone
+		 * @param counter the timer counter value to use to produce the required tone
+		 * @param ms the duration, in milliseconds, of the tone to generate
+		 * 
+		 * @start_tone(PRESCALER, COUNTER)
+		 */
+		inline void tone(PRESCALER prescaler, COUNTER counter, uint16_t ms)
+		{
+			generator_.start_frequency(prescaler, counter);
+			pause(ms);
+		}
+		
 	private:
 		SQWGEN generator_;
 	};
 }
 
 #endif /* TONES_HH */
+/// @endcond
