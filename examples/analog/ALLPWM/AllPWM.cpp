199608368ccc1c5eb6b4c047736a34189eec8bf0
jfpoilpret <jfpoilpret@gmail.com>
2019-05-18 22:02:37 +0200

    Add new example to test all available PWM pins on each target MCU.

---
+++ examples/analog/ALLPWM/AllPWM.cpp
@@ -0,0 +1,116 @@
+//   Copyright 2016-2019 Jean-Francois Poilpret
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+/*
+ * Check all PWM pins on the built target.
+ * For each PWM pin, a LED is lit from 0% to 100%, then switched off.
+ * TODO review after program written for all targets
+ * Wiring:
+ * - connect a LED to each PWM pin (detailed later) and a 1K resistor to GND
+ * - on ATmega328P based boards (including Arduino UNO):
+ *   - D3, D5, D6, D9, D10, D11
+ * - on Arduino LENOARDO:
+ *   - D3, D5, D9, D10, D11
+ * - on Arduino MEGA:
+ *   - D2-D13, D44-46
+ * - on ATtinyX4 based boards:
+ *   - PA5-7, PB2
+ * - on ATtinyX5 based boards:
+ *   - PB0, PB1
+ */
+
+#include <fastarduino/time.h>
+#include <fastarduino/timer.h>
+#include <fastarduino/pwm.h>
+
+using namespace board;
+
+// Frequency for PWM
+constexpr const uint16_t PWM_FREQUENCY = 450;
+
+template<Timer NTIMER, PWMPin PIN> void check_PWM()
+{
+	using TIMER = timer::Timer<NTIMER>;
+	using CALC = timer::Calculator<NTIMER>;
+	using LED_OUTPUT = analog::PWMOutput<PIN>;
+	constexpr const TIMER::PRESCALER PRESCALER = CALC::FastPWM_prescaler(PWM_FREQUENCY);
+
+	TIMER timer{timer::TimerMode::FAST_PWM, PRESCALER};
+	LED_OUTPUT led{timer};
+	// Start timer
+	timer.begin();
+	// Loop of samplings
+	using TYPE = LED_OUTPUT::TYPE;
+	constexpr const MAX = LED_OUTPUT::MAX;
+	for (uint16_t duty = 0; duty < MAX; duty += MAX / 10)
+	{
+		led.set_duty(TYPE(duty));
+		time::delay_ms(1000);
+	}
+	led.set_duty(0);
+	timer.end();
+}
+
+
+int main()
+{
+	init();
+
+	// Enable interrupts
+	sei();
+
+#if defined(ARDUINO_UNO) || defined(BREADBOARD_ATMEGA328P) || defined(ARDUINO_NANO)
+	check_PWM<Timer::TIMER0, PWMPin::D6_PD6_OC0A>();
+	check_PWM<Timer::TIMER0, PWMPin::D5_PD5_OC0B>();
+	check_PWM<Timer::TIMER1, PWMPin::D9_PB1_OC1A>();
+	check_PWM<Timer::TIMER1, PWMPin::D10_PB2_OC1B>();
+	check_PWM<Timer::TIMER2, PWMPin::D11_PB3_OC2A>();
+	check_PWM<Timer::TIMER2, PWMPin::D3_PD3_OC2B>();
+#elif defined (ARDUINO_LEONARDO)
+	check_PWM<Timer::TIMER0, PWMPin::D11_PB7_OC0A>();
+	check_PWM<Timer::TIMER0, PWMPin::D3_PD0_OC0B>();
+	check_PWM<Timer::TIMER1, PWMPin::D9_PB5_OC1A>();
+	check_PWM<Timer::TIMER1, PWMPin::D10_PB6_OC1B>();
+	check_PWM<Timer::TIMER1, PWMPin::D11_PB7_OC1C>();
+	check_PWM<Timer::TIMER3, PWMPin::D5_PC6_OC3A>();
+#elif defined (ARDUINO_MEGA)
+	check_PWM<Timer::TIMER0, PWMPin::D13_PB7_OC0A>();
+	check_PWM<Timer::TIMER0, PWMPin::D4_PG5_OC0B>();
+	check_PWM<Timer::TIMER1, PWMPin::D11_PB5_OC1A>();
+	check_PWM<Timer::TIMER1, PWMPin::D12_PB6_OC1B>();
+	check_PWM<Timer::TIMER1, PWMPin::D13_PB7_OC1C>();
+	check_PWM<Timer::TIMER2, PWMPin::D10_PB4_OC2A>();
+	check_PWM<Timer::TIMER2, PWMPin::D9_PH6_OC2B>();
+	check_PWM<Timer::TIMER3, PWMPin::D5_PE3_OC3A>();
+	check_PWM<Timer::TIMER3, PWMPin::D2_PE4_OC3B>();
+	check_PWM<Timer::TIMER3, PWMPin::D3_PE5_OC3C>();
+	check_PWM<Timer::TIMER4, PWMPin::D6_PH3_OC4A>();
+	check_PWM<Timer::TIMER4, PWMPin::D7_PH4_OC4B>();
+	check_PWM<Timer::TIMER4, PWMPin::D8_PH5_OC4C>();
+	check_PWM<Timer::TIMER5, PWMPin::D46_PL3_OC5A>();
+	check_PWM<Timer::TIMER5, PWMPin::D45_PL4_OC5B>();
+	check_PWM<Timer::TIMER5, PWMPin::D44_PL5_OC5C>();
+#elif defined (BREADBOARD_ATTINYX4)
+	check_PWM<Timer::TIMER0, PWMPin::D10_PB2_OC0A>();
+	check_PWM<Timer::TIMER0, PWMPin::D7_PA7_OC0B>();
+	check_PWM<Timer::TIMER1, PWMPin::D6_PA6_OC1A>();
+	check_PWM<Timer::TIMER1, PWMPin::D5_PA5_OC1B>();
+#elif defined (BREADBOARD_ATTINYX5)
+	check_PWM<Timer::TIMER0, PWMPin::D0_PB0_OC0A>();
+	check_PWM<Timer::TIMER0, PWMPin::D1_PB1_OC0B>();
+#else
+#error "Current target is not yet supported!"
+#endif
+	return 0;
+}
