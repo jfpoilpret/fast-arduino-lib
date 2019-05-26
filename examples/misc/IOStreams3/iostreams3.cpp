13f0556688fbedd4333f6196f1274b9076357b2a
jfpoilpret <jfpoilpret@gmail.com>
2019-05-26 13:01:16 +0200

    Update IOStream example to show issue #33

--- examples/misc/IOStreams3/iostreams3.cpp
+++ examples/misc/IOStreams3/iostreams3.cpp
@@ -30,8 +30,8 @@ REGISTER_UART_ISR(0)
 #endif
 
 // Buffers for UART
-static const uint8_t INPUT_BUFFER_SIZE = 64;
-static const uint8_t OUTPUT_BUFFER_SIZE = 64;
+static const uint8_t INPUT_BUFFER_SIZE = 16;
+static const uint8_t OUTPUT_BUFFER_SIZE = 32;
 static char input_buffer[INPUT_BUFFER_SIZE];
 static char output_buffer[OUTPUT_BUFFER_SIZE];
 
@@ -82,8 +82,7 @@ int main()
 
 	// Show out state after overflowed output
 	out	<< "abcdefghijklmnopqrstuvwxyz" 
-		<< "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-		<< "abcdefghijklmnopqrstuvwxyz\n";
+		<< "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
 	ios::iostate state = out.rdstate();
 	out << flush << endl;
 	trace_state(out, state);
@@ -107,7 +106,7 @@ int main()
 	trace_state(out, in);
 
 	// Show in state when trying to get() a character but streambuf currently empty
-	out << "Getting next character (should be EOF)" << endl;
+	out << "Enter EOF" << endl;
 	int val = in.get();
 	out << hex << val << endl;
 	trace_state(out, in);
