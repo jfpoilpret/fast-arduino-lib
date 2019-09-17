898ca29f2df9f76ffdb559ab2816c25d36e84f0a
jfpoilpret <jfpoilpret@gmail.com>
2019-09-18 00:32:28 +0200

    Improve tone player example by using 2 simultaneous channels

--- examples/tones/tones6/Tones6.cpp
+++ examples/tones/tones6/Tones6.cpp
@@ -13,20 +13,22 @@
 //   limitations under the License.
 
 /*
- * Frequency generator example, used to play the Imperial March.
+ * Frequency generator example, used to play the Imperial March on 2 channels.
  * This example is playing the melody asynchronously, based on RTT events.
  * In this example, the melody is stored in Flash.
  * 
  * Wiring:
  * - on Arduino UNO:
- *   - D6: connect to a 5V piezo buzzer with the othe lead connected to ground
- *   - D13: embedded LED that blinks synchronously from main()
+ *   - D6: connect to a 5V piezo buzzer (treble) with the othe lead connected to ground
+ *   - D11: connect to a 5V piezo buzzer (bass) with the othe lead connected to ground
  */
 
 // Imperial march tones thanks:
 // http://processors.wiki.ti.com/index.php/Playing_The_Imperial_March
 // Better score (simplified) found at
 // http://www.filmmusicnotes.com/john-williams-themes-part-3-of-6-the-imperial-march-darth-vaders-theme/
+// March score (timpani) thanks:
+// http://pop-sheet-music.com/Files/9763704e1f9e5a8fd0492a98cd4b3e54.pdf
 
 // Example of square wave generation, using CTC mode and COM toggle
 #include <fastarduino/events.h>
@@ -37,17 +39,22 @@
 #include <fastarduino/devices/tone_player.h>
 
 // Board-dependent settings
-static constexpr const board::Timer NTIMER = board::Timer::TIMER0;
-static constexpr const board::PWMPin OUTPUT = board::PWMPin::D6_PD6_OC0A;
+static constexpr const board::Timer NTIMER1 = board::Timer::TIMER0;
+static constexpr const board::PWMPin OUTPUT1 = board::PWMPin::D6_PD6_OC0A;
+static constexpr const board::Timer NTIMER2 = board::Timer::TIMER2;
+static constexpr const board::PWMPin OUTPUT2 = board::PWMPin::D11_PB3_OC2A;
+
 #define RTTTIMER 1
 static constexpr const board::Timer NRTTTIMER = board::Timer::TIMER1;
 
-using TONEPLAYER = devices::audio::AsyncTonePlayer<NTIMER, OUTPUT>;
-using devices::audio::Tone;
-using namespace devices::audio::SpecialTone;
-using QTONEPLAY = TONEPLAYER::TONE_PLAY;
+using TONEPLAYER1 = devices::audio::AsyncTonePlayer<NTIMER1, OUTPUT1>;
+using QTONEPLAY1 = TONEPLAYER1::TONE_PLAY;
+using TONEPLAYER2 = devices::audio::AsyncTonePlayer<NTIMER2, OUTPUT2>;
+using QTONEPLAY2 = TONEPLAYER2::TONE_PLAY;
 
 // Define constants with short names to ease score transcription
+using devices::audio::Tone;
+using namespace devices::audio::SpecialTone;
 using devices::audio::Duration;
 static constexpr const Duration WN = Duration::WHOLE;
 static constexpr const Duration HN = Duration::HALF;
@@ -55,101 +62,179 @@ static constexpr const Duration QN = Duration::QUARTER;
 static constexpr const Duration QV = Duration::QUAVER;
 static constexpr const Duration SQ = Duration::SEMI_QUAVER;
 static constexpr auto DOT = devices::audio::dotted;
+static constexpr auto TRIPLET = devices::audio::triplet;
 
-static const QTONEPLAY music[] PROGMEM =
+static const QTONEPLAY1 music1[] PROGMEM =
 {
-	// First part
-	QTONEPLAY{Tone::G2, QN},
-	QTONEPLAY{Tone::G2, QN},
-	QTONEPLAY{Tone::G2, QN},
-	QTONEPLAY{Tone::Ef2, DOT(QV)},
-	QTONEPLAY{Tone::Bf2, SQ},
+	// Prelude: march (4 bars)
+	QTONEPLAY1{Tone::REST, WN},
+	QTONEPLAY1{Tone::REST, WN},
+	QTONEPLAY1{Tone::REST, WN},
+	QTONEPLAY1{Tone::REST, WN},
 
-	QTONEPLAY{Tone::G2, QN},
-	QTONEPLAY{Tone::Ef2, DOT(QV)},
-	QTONEPLAY{Tone::Bf2, SQ},
-	QTONEPLAY{Tone::G2, HN},
+	// First part
+	QTONEPLAY1{Tone::G2, QN},
+	QTONEPLAY1{Tone::G2, QN},
+	QTONEPLAY1{Tone::G2, QN},
+	QTONEPLAY1{Tone::Ef2, DOT(QV)},
+	QTONEPLAY1{Tone::Bf2, SQ},
+
+	QTONEPLAY1{Tone::G2, QN},
+	QTONEPLAY1{Tone::Ef2, DOT(QV)},
+	QTONEPLAY1{Tone::Bf2, SQ},
+	QTONEPLAY1{Tone::G2, HN},
 
 	// Second part
-	QTONEPLAY{Tone::D3, QN},
-	QTONEPLAY{Tone::D3, QN},
-	QTONEPLAY{Tone::D3, QN},
-	QTONEPLAY{Tone::Ef3, DOT(QV)},
-	QTONEPLAY{Tone::Bf2, SQ},
+	QTONEPLAY1{Tone::D3, QN},
+	QTONEPLAY1{Tone::D3, QN},
+	QTONEPLAY1{Tone::D3, QN},
+	QTONEPLAY1{Tone::Ef3, DOT(QV)},
+	QTONEPLAY1{Tone::Bf2, SQ},
 
-	QTONEPLAY{Tone::Gf2, QN},
-	QTONEPLAY{Tone::Ef2, DOT(QV)},
-	QTONEPLAY{Tone::Bf2, SQ},
-	QTONEPLAY{Tone::G2, HN},
+	QTONEPLAY1{Tone::Gf2, QN},
+	QTONEPLAY1{Tone::Ef2, DOT(QV)},
+	QTONEPLAY1{Tone::Bf2, SQ},
+	QTONEPLAY1{Tone::G2, HN},
 
 	// Third part
-	QTONEPLAY{Tone::G3, QN},
-	QTONEPLAY{Tone::G2, DOT(QV)},
-	QTONEPLAY{Tone::G2, SQ},
-	QTONEPLAY{Tone::G3, QN},
-	QTONEPLAY{Tone::Fs3, DOT(QV)},
-	QTONEPLAY{Tone::F3, SQ},
+	QTONEPLAY1{Tone::G3, QN},
+	QTONEPLAY1{Tone::G2, DOT(QV)},
+	QTONEPLAY1{Tone::G2, SQ},
+	QTONEPLAY1{Tone::G3, QN},
+	QTONEPLAY1{Tone::Fs3, DOT(QV)},
+	QTONEPLAY1{Tone::F3, SQ},
 
-	QTONEPLAY{Tone::E3, SQ},
-	QTONEPLAY{Tone::Ds3, SQ},
-	QTONEPLAY{Tone::E3, QV},
-	QTONEPLAY{Tone::SILENCE, QV},
-	QTONEPLAY{Tone::Gs2, QV},
-	QTONEPLAY{Tone::Cs3, QN},
-	QTONEPLAY{Tone::C3, DOT(QV)},
-	QTONEPLAY{Tone::B2, SQ},
+	QTONEPLAY1{Tone::E3, SQ},
+	QTONEPLAY1{Tone::Ds3, SQ},
+	QTONEPLAY1{Tone::E3, QV},
+	QTONEPLAY1{Tone::SILENCE, QV},
+	QTONEPLAY1{Tone::Gs2, QV},
+	QTONEPLAY1{Tone::Cs3, QN},
+	QTONEPLAY1{Tone::C3, DOT(QV)},
+	QTONEPLAY1{Tone::B2, SQ},
 
-	QTONEPLAY{Tone::Bf2, SQ},
-	QTONEPLAY{Tone::A2, SQ},
-	QTONEPLAY{Tone::Bf2, QV},
-	QTONEPLAY{Tone::SILENCE, QV},
-	QTONEPLAY{Tone::Ef2, SQ},
-	QTONEPLAY{Tone::Gf2, QN},
-	QTONEPLAY{Tone::Ef2, DOT(QV)},
-	QTONEPLAY{Tone::Gf2, SQ},
+	QTONEPLAY1{Tone::Bf2, SQ},
+	QTONEPLAY1{Tone::A2, SQ},
+	QTONEPLAY1{Tone::Bf2, QV},
+	QTONEPLAY1{Tone::SILENCE, QV},
+	QTONEPLAY1{Tone::Ef2, SQ},
+	QTONEPLAY1{Tone::Gf2, QN},
+	QTONEPLAY1{Tone::Ef2, DOT(QV)},
+	QTONEPLAY1{Tone::Gf2, SQ},
 
-	QTONEPLAY{Tone::Bf2, QN},
-	QTONEPLAY{Tone::G2, DOT(QV)},
-	QTONEPLAY{Tone::Bf2, SQ},
-	QTONEPLAY{Tone::D3, HN},
+	QTONEPLAY1{Tone::Bf2, QN},
+	QTONEPLAY1{Tone::G2, DOT(QV)},
+	QTONEPLAY1{Tone::Bf2, SQ},
+	QTONEPLAY1{Tone::D3, HN},
 
 	// Fourth part (like 3rd part except last bar)
-	QTONEPLAY{Tone::G3, QN},
-	QTONEPLAY{Tone::G2, DOT(QV)},
-	QTONEPLAY{Tone::G2, SQ},
-	QTONEPLAY{Tone::G3, QN},
-	QTONEPLAY{Tone::Fs3, DOT(QV)},
-	QTONEPLAY{Tone::F3, SQ},
+	QTONEPLAY1{Tone::G3, QN},
+	QTONEPLAY1{Tone::G2, DOT(QV)},
+	QTONEPLAY1{Tone::G2, SQ},
+	QTONEPLAY1{Tone::G3, QN},
+	QTONEPLAY1{Tone::Fs3, DOT(QV)},
+	QTONEPLAY1{Tone::F3, SQ},
 
-	QTONEPLAY{Tone::E3, SQ},
-	QTONEPLAY{Tone::Ds3, SQ},
-	QTONEPLAY{Tone::E3, QV},
-	QTONEPLAY{Tone::SILENCE, QV},
-	QTONEPLAY{Tone::Gs2, QV},
-	QTONEPLAY{Tone::Cs3, QN},
-	QTONEPLAY{Tone::C3, DOT(QV)},
-	QTONEPLAY{Tone::B2, SQ},
+	QTONEPLAY1{Tone::E3, SQ},
+	QTONEPLAY1{Tone::Ds3, SQ},
+	QTONEPLAY1{Tone::E3, QV},
+	QTONEPLAY1{Tone::SILENCE, QV},
+	QTONEPLAY1{Tone::Gs2, QV},
+	QTONEPLAY1{Tone::Cs3, QN},
+	QTONEPLAY1{Tone::C3, DOT(QV)},
+	QTONEPLAY1{Tone::B2, SQ},
 
-	QTONEPLAY{Tone::Bf2, SQ},
-	QTONEPLAY{Tone::A2, SQ},
-	QTONEPLAY{Tone::Bf2, QV},
-	QTONEPLAY{Tone::SILENCE, QV},
-	QTONEPLAY{Tone::Ef2, SQ},
-	QTONEPLAY{Tone::Gf2, QN},
-	QTONEPLAY{Tone::Ef2, DOT(QV)},
-	QTONEPLAY{Tone::Gf2, SQ},
+	QTONEPLAY1{Tone::Bf2, SQ},
+	QTONEPLAY1{Tone::A2, SQ},
+	QTONEPLAY1{Tone::Bf2, QV},
+	QTONEPLAY1{Tone::SILENCE, QV},
+	QTONEPLAY1{Tone::Ef2, SQ},
+	QTONEPLAY1{Tone::Gf2, QN},
+	QTONEPLAY1{Tone::Ef2, DOT(QV)},
+	QTONEPLAY1{Tone::Bf2, SQ},
 
-	QTONEPLAY{Tone::G2, QN},
-	QTONEPLAY{Tone::Ef2, DOT(QV)},
-	QTONEPLAY{Tone::Bf2, SQ},
-	QTONEPLAY{Tone::G2, HN},
+	QTONEPLAY1{Tone::G2, QN},
+	QTONEPLAY1{Tone::Ef2, DOT(QV)},
+	QTONEPLAY1{Tone::Bf2, SQ},
+	QTONEPLAY1{Tone::G2, HN},
 
-	QTONEPLAY{END, 0}
+	QTONEPLAY1{END, 0}
 };
 
-static constexpr const uint8_t BPM = 120;
+// March melody (each part is one bar), melody starts after 4 bars
+static const QTONEPLAY2 music2[] PROGMEM =
+{
+	// First part
+	QTONEPLAY2(REPEAT_START, 0),
+	QTONEPLAY2{Tone::G0, QN},
+	QTONEPLAY2{Tone::G0, QV},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, QV},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::Ef0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::Ef0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::Ef0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::Ef0, QV},
+	QTONEPLAY2(REPEAT_END, 1),
 
-using GENERATOR = TONEPLAYER::GENERATOR;
+	// 2nd part
+	QTONEPLAY2(REPEAT_START, 0),
+	QTONEPLAY2{Tone::G0, QN},
+	QTONEPLAY2{Tone::G0, QV},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, QV},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, QV},
+	QTONEPLAY2(REPEAT_END, 4),
+
+	// 3rd part
+	QTONEPLAY2{Tone::Ef0, QN},
+	QTONEPLAY2{Tone::C1, QN},
+	QTONEPLAY2{Tone::G0, QV},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, QV},
+
+	// 4th part (like 2nd part)
+	QTONEPLAY2{Tone::G0, QN},
+	QTONEPLAY2{Tone::G0, QV},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, QV},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, TRIPLET(SQ)},
+	QTONEPLAY2{Tone::G0, QV},
+
+	// Followed by 12 bars melody...
+	//TODO add additional march bars here
+
+	QTONEPLAY2{END, 0}
+};
+
+static constexpr const uint8_t BPM = 112;
+
+using GENERATOR1 = TONEPLAYER1::GENERATOR;
+using GENERATOR2 = TONEPLAYER2::GENERATOR;
 
 using RTT = timer::RTT<NRTTTIMER>;
 
@@ -168,26 +253,30 @@ int main()
 {
 	sei();
 
-	gpio::FastPinType<board::DigitalPin::LED>::TYPE led{gpio::PinMode::OUTPUT};
-	
 	RTT timer;
 	RTTCALLBACK handler{events_queue};
 	interrupt::register_handler(handler);
 
-	GENERATOR generator;
-	TONEPLAYER player{generator};
+	GENERATOR1 generator1;
+	TONEPLAYER1 player1{generator1};
+	GENERATOR2 generator2;
+	TONEPLAYER2 player2{generator2};
 
 	time::delay_ms(1000);
-	player.play_flash(music, BPM);
+	player1.play_flash(music1, BPM);
+	player2.play_flash(music2, BPM);
 
 	timer.begin();
 	while (true)
 	{
+		if ((!player1.is_playing()) && (!player2.is_playing())) break;
 		EVENT event;
-		if (events_queue.pull(event) && player.is_playing())
+		if (events_queue.pull(event))
 		{
-			led.toggle();
-			player.update(timer.millis());
+			if (player1.is_playing())
+				player1.update(timer.millis());
+			if (player2.is_playing())
+				player2.update(timer.millis());
 		}
 	}
 }
