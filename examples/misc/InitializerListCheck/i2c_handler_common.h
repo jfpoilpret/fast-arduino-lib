94ab51c792002f52356f77c90e2d349305c0d558
jfpoilpret <jfpoilpret@gmail.com>
2020-06-06 16:35:33 +0200

    Add unit tests for initializer list.

---
+++ examples/misc/InitializerListCheck/i2c_handler_common.h
@@ -0,0 +1,134 @@
+//   Copyright 2016-2020 Jean-Francois Poilpret
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+/// @cond api
+
+#ifndef I2C_HANDLER_COMMON_HH
+#define I2C_HANDLER_COMMON_HH
+
+#include <stdint.h>
+#include <fastarduino/i2c.h>
+#include <fastarduino/streams.h>
+
+namespace i2c
+{
+	// Type of commands in queue
+	class I2CCommandType
+	{
+	public:
+		constexpr I2CCommandType()
+			: none{true}, write{false}, force_stop{false}, finish_future{false} {}
+		constexpr I2CCommandType(const I2CCommandType&) = default;
+		constexpr I2CCommandType& operator=(const I2CCommandType&) = default;
+
+	private:
+		constexpr I2CCommandType(bool write, bool force_stop, bool finish_future)
+			: none{false}, write{write}, force_stop{force_stop}, finish_future{finish_future} {}
+
+		// true if this is an empty command
+		bool none : 1;
+		// true if this is a write command, false for a read command
+		bool write : 1;
+		// true if a STOP condition must absolutely be forced at the end of this command
+		bool force_stop : 1;
+		// true if aasociated future is void and must eb forced finished after this command
+		bool finish_future : 1;
+
+		friend class I2CCommand;
+		friend streams::ostream& operator<<(streams::ostream&, const I2CCommandType&);
+		friend bool operator==(const I2CCommandType&, const I2CCommandType&);
+		friend bool operator!=(const I2CCommandType&, const I2CCommandType&);
+	};
+
+	streams::ostream& operator<<(streams::ostream& out, const I2CCommandType& t)
+	{
+		if (t.none) return out << F("NONE") << streams::flush;
+		out << (t.write ? F("WRITE") : F("READ"));
+		if (t.force_stop)
+			out << F("[STOP]");
+		if (t.finish_future)
+			out << F("[FINISH]");
+		return out << streams::flush;
+	}
+	bool operator==(const I2CCommandType& a, const I2CCommandType& b)
+	{
+		return	(a.none == b.none)
+			&&	(a.write == b.write)
+			&&	(a.force_stop == b.force_stop)
+			&&	(a.finish_future == b.finish_future);
+	}
+	bool operator!=(const I2CCommandType& a, const I2CCommandType& b)
+	{
+		return	(a.none != b.none)
+			||	(a.write != b.write)
+			||	(a.force_stop != b.force_stop)
+			||	(a.finish_future != b.finish_future);
+	}
+
+	// Command in the queue
+	class I2CCommand
+	{
+	public:
+		constexpr I2CCommand() = default;
+		constexpr I2CCommand(const I2CCommand&) = default;
+		constexpr I2CCommand& operator=(const I2CCommand&) = default;
+
+		static constexpr I2CCommand none()
+		{
+			return I2CCommand{};
+		}
+
+		static constexpr I2CCommand read(uint8_t target, bool force_stop, uint8_t future_id, bool finish_future)
+		{
+			return I2CCommand{I2CCommandType{false, force_stop, finish_future}, target, future_id};
+		}
+		static constexpr I2CCommand write(uint8_t target, bool force_stop, uint8_t future_id, bool finish_future)
+		{
+			return I2CCommand{I2CCommandType{true, force_stop, finish_future}, target, future_id};
+		}
+
+	private:
+		constexpr I2CCommand(I2CCommandType type, uint8_t target, uint8_t future_id)
+			: type{type}, target{target}, future_id{future_id} {}
+
+		// Type of this command
+		I2CCommandType type = I2CCommandType{};
+		// Address of the target device (on 8 bits, already left-shifted)
+		uint8_t target = 0;
+		uint8_t future_id = 0;
+
+		friend streams::ostream& operator<<(streams::ostream&, const I2CCommand&);
+		friend bool operator==(const I2CCommand&, const I2CCommand&);
+		friend bool operator!=(const I2CCommand&, const I2CCommand&);
+	};
+
+	streams::ostream& operator<<(streams::ostream& out, const I2CCommand& c)
+	{
+		out	<< '{' << c.type << ',' 
+			<< streams::hex << c.target << ',' 
+			<< streams::dec << c.future_id << '}' << streams::flush;
+		return out;
+	}
+	bool operator==(const I2CCommand& a, const I2CCommand& b)
+	{
+		return (a.type == b.type) && (a.target == b.target) && (a.future_id == b.future_id);
+	}
+	bool operator!=(const I2CCommand& a, const I2CCommand& b)
+	{
+		return (a.type != b.type) || (a.target != b.target) || (a.future_id != b.future_id);
+	}
+}
+
+#endif /* I2C_HANDLER_COMMON_HH */
+/// @endcond
---
+++ examples/misc/InitializerListCheck/initializerlistcheck.cpp
@@ -0,0 +1,142 @@
+//   Copyright 2016-2020 Jean-Francois Poilpret
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+/*
+ * Special check of initializer_list (kind of unit tests).
+ * Wiring:
+ * - on Arduino UNO
+ *   - Standard USB to console
+ * - on ATtinyX4 based boards
+ *   - D8 (PB0): TX conencted to a Serial2USB converter
+ */
+
+#include <fastarduino/flash.h>
+#include <fastarduino/initializer_list.h>
+#include <fastarduino/streams.h>
+#include <fastarduino/iomanip.h>
+#include <fastarduino/tests/assertions.h>
+
+#include "i2c_handler_common.h"
+
+#ifdef ARDUINO_UNO
+#define HARD_UART
+#include <fastarduino/uart.h>
+static constexpr const board::USART UART = board::USART::USART0;
+static constexpr const uint8_t OUTPUT_BUFFER_SIZE = 128;
+// Define vectors we need in the example
+REGISTER_UATX_ISR(0)
+#elif defined (BREADBOARD_ATTINYX4)
+#include <fastarduino/soft_uart.h>
+static constexpr const board::DigitalPin TX = board::DigitalPin::D8_PB0;
+static constexpr const uint8_t OUTPUT_BUFFER_SIZE = 64;
+#else
+#error "Current target is not yet supported!"
+#endif
+
+using namespace streams;
+
+// Buffers for UART
+//==================
+static char output_buffer[OUTPUT_BUFFER_SIZE];
+
+// Utilities for assertions and traces
+//=====================================
+
+template<typename T, uint8_t SIZE>
+void assert(ostream& out, const flash::FlashStorage* message, const T (&expected)[SIZE], std::initializer_list<T> actual)
+{
+	out << message << endl;
+	assert(out, F("size"), SIZE, actual.size());
+	const uint8_t size = (SIZE <= actual.size() ? SIZE : actual.size());
+	uint8_t i = 0;
+	for (T t : actual)
+	{
+		out << t << endl;
+		assert(out, "", expected[i], t);
+		if (++i == size) break;
+	}
+}
+
+namespace i2c
+{
+	enum class I2CFinish : uint8_t
+	{
+		NONE = 0,
+		FORCE_STOP = 0x01,
+		FUTURE_FINISH = 0x02
+	};
+	I2CFinish operator|(I2CFinish f1, I2CFinish f2)
+	{
+		return I2CFinish(uint8_t(f1) | uint8_t(f2));
+	}
+	I2CFinish operator&(I2CFinish f1, I2CFinish f2)
+	{
+		return I2CFinish(uint8_t(f1) & uint8_t(f2));
+	}
+}
+
+using namespace i2c;
+
+static constexpr const uint8_t DEVICE_ADDRESS = 0x68 << 1;
+I2CCommand read(I2CFinish finish = I2CFinish::NONE)
+{
+	return I2CCommand::read(
+		DEVICE_ADDRESS, uint8_t(finish & I2CFinish::FORCE_STOP), 0, uint8_t(finish & I2CFinish::FUTURE_FINISH));
+}
+I2CCommand write(I2CFinish finish = I2CFinish::NONE)
+{
+	return I2CCommand::write(
+		DEVICE_ADDRESS, uint8_t(finish & I2CFinish::FORCE_STOP), 0, uint8_t(finish & I2CFinish::FUTURE_FINISH));
+}
+
+int main()
+{
+	board::init();
+
+	// Enable interrupts at startup time
+	sei();
+
+	// Initialize debugging output
+#ifdef HARD_UART
+	serial::hard::UATX<UART> uatx{output_buffer};
+#else
+	serial::soft::UATX<TX> uatx{output_buffer};
+#endif
+	uatx.begin(115200);
+	ostream out = uatx.out();
+	out << boolalpha << showbase;
+
+	// {
+	// 	const char expected[0] = {};
+	// 	std::initializer_list<char> actual = {};
+	// 	assert(out, F("check char[]"), expected, actual);
+	// }
+
+	{
+		const char expected[] = {'a', 'b', 'c', 'd', 'e'};
+		assert(out, F("check char[]"), expected, {'a', 'b', 'c', 'd', 'e'});
+	}
+
+	{
+		const int expected[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		assert(out, F("check int[]"), expected, {0, 1, 2 , 3, 4, 5, 6, 7, 8, 9});
+	}
+
+	{
+		I2CCommand expected[] = {write(), read(I2CFinish::FORCE_STOP)};
+		assert(out, F("check I2CCommand[]"), expected, {write(), read(I2CFinish::FORCE_STOP)});
+	}
+
+	out << endl;
+}
