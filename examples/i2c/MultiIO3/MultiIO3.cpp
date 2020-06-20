014b26354b3012df170831483663aae24c834dd0
jfpoilpret <jfpoilpret@gmail.com>
2020-06-20 11:03:32 +0200

    Port 3rd MCP23017 example to new I2C.

--- examples/i2c/MultiIO3/MultiIO3.cpp
+++ examples/i2c/MultiIO3/MultiIO3.cpp
@@ -34,8 +34,13 @@
 
 #include <fastarduino/int.h>
 #include <fastarduino/time.h>
-#include <fastarduino/i2c_device.h>
-#include <fastarduino/devices/mcp23017.h>
+#include <fastarduino/new_i2c_device.h>
+#include <fastarduino/devices/new_mcp23017.h>
+
+static constexpr uint8_t I2C_BUFFER_SIZE = 32;
+static constexpr uint8_t MAX_FUTURES = 128;
+static i2c::I2CCommand i2c_buffer[I2C_BUFFER_SIZE];
+REGISTER_I2C_ISR(i2c::I2CMode::FAST)
 
 constexpr const board::ExternalInterruptPin INT_PIN = board::ExternalInterruptPin::D2_PD2_EXT0;
 constexpr const i2c::I2CMode I2C_MODE = i2c::I2CMode::FAST;
@@ -101,7 +106,11 @@ int main()
 	
 	// Start TWI interface
 	//====================
-	i2c::I2CManager<I2C_MODE> manager;
+	// Initialize FutureManager
+	future::FutureManager<MAX_FUTURES> future_manager;
+
+	// Initialize I2C async handler
+	MCP::MANAGER manager{i2c_buffer, i2c::I2CErrorPolicy::CLEAR_ALL_COMMANDS};
 	manager.begin();
 
 	// Initialize chip
