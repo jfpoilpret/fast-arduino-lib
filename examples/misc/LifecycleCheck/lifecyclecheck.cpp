48ee1e7cf9de6a407fb579a440df6a638a1cb2cc
jfpoilpret <jfpoilpret@gmail.com>
2020-07-10 21:22:10 +0200

    Move LC API test program and include to make.

--- examples/misc/LifecycleCheck/lifecyclecheck.cpp
+++ examples/misc/LifecycleCheck/lifecyclecheck.cpp
@@ -1,6 +1,22 @@
+//   Copyright 2016-2020 Jean-Francois Poilpret
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 /*
- * This program is just for personal experiments here on C++ mixin for 
- * handling lifecycle of specific objects.
+ * Special check for LifeCycle and Proxy API (kind of unit tests).
+ * Wiring:
+ * - Arduino UNO
+ *   - Standard USB to console
  */
 
 #include <fastarduino/tests/assertions.h>
@@ -10,6 +26,9 @@
 #include <fastarduino/move.h>
 #include <fastarduino/lifecycle.h>
 
+#ifndef ARDUINO_UNO
+#error "Current target is not yet supported!"
+#endif
 // Register vector for UART (used for debug)
 REGISTER_UATX_ISR(0)
 
