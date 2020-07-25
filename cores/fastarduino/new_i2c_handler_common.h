f749bb945e8c4cce48ec69940f6cd3d0968da4ed
jfpoilpret <jfpoilpret@gmail.com>
2020-07-25 15:00:57 +0200

    Add API doc.

--- cores/fastarduino/new_i2c_handler_common.h
+++ cores/fastarduino/new_i2c_handler_common.h
@@ -54,14 +54,20 @@ namespace i2c
 		CLEAR_TRANSACTION_COMMANDS
 	};
 
-	// Used by TWI ISR to potentially call a registered callback
 	//TODO move to ATmega?
-	//TODO DOC
+	/**
+	 * Type passed to I2C ISR registered callbacks (ATmega MCU only) when an 
+	 * asynchronous I2C transaction is executed. 
+	 */
 	enum class I2CCallback : uint8_t
 	{
+		/** An I2C command is being processed (intermediate step). */
 		NONE = 0,
+		/** An I2C command has just been finished executed. */
 		END_COMMAND,
+		/** The last I2C command in a transaction ahs just been finished executing. */
 		END_TRANSACTION,
+		/** An error has occurred during I2C transaction execution. */
 		ERROR
 	};
 
