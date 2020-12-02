4c790aa8fba25f99b43e372718411a3ddf16202c
jfpoilpret <jfpoilpret@gmail.com>
2020-12-02 18:18:04 +0100

    Early implementation of VL53L0X support (only 4 simple GET API).

---
+++ examples/i2c/ToF1/ToF1.cpp
@@ -0,0 +1,139 @@
+//   Copyright 2016-2020 Jean-Francois Poilpret
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+/*
+ * Simple ranger example, using VL53L0X Time-of-flight range sensor I2C device.
+ * This program uses FastArduino VL53L0X support API.
+ * 
+ * Wiring:
+ * TODO (not sure)  NB: you should add pullup resistors (10K-22K typically) on both SDA and SCL lines.
+ * - on ATmega328P based boards (including Arduino UNO):
+ *   - A4 (PC4, SDA): connected to VL53L0X SDA pin
+ *   - A5 (PC5, SCL): connected to VL53L0X SCL pin
+ *   - direct USB access
+ */
+
+#include <fastarduino/time.h>
+#include <fastarduino/uart.h>
+#include <fastarduino/i2c_handler.h>
+#include <fastarduino/i2c_debug.h>
+#include <fastarduino/i2c_status.h>
+#include <fastarduino/devices/vl53l0x.h>
+
+#define DEBUG_I2C
+#define FORCE_SYNC
+
+static constexpr const board::USART UART = board::USART::USART0;
+static constexpr const uint8_t OUTPUT_BUFFER_SIZE = 64;
+static char output_buffer[OUTPUT_BUFFER_SIZE];
+
+REGISTER_UATX_ISR(0)
+
+#ifdef DEBUG_I2C
+static constexpr const uint8_t DEBUG_SIZE = 64;
+using DEBUGGER = i2c::debug::I2CDebugStatusRecorder<DEBUG_SIZE, DEBUG_SIZE>;
+#	if I2C_TRUE_ASYNC and not defined(FORCE_SYNC)
+using MANAGER = i2c::I2CAsyncStatusDebugManager<
+	i2c::I2CMode::FAST, i2c::I2CErrorPolicy::CLEAR_ALL_COMMANDS, DEBUGGER&, DEBUGGER&>;
+static MANAGER::I2CCOMMAND i2c_buffer[I2C_BUFFER_SIZE];
+#	else
+using MANAGER = i2c::I2CSyncStatusDebugManager<i2c::I2CMode::FAST, DEBUGGER&, DEBUGGER&>;
+#	endif
+#define DEBUG(OUT) debugger.trace(OUT)
+#define RESET_DEBUG() debugger.reset()
+
+#else
+
+#	if I2C_TRUE_ASYNC and not defined(FORCE_SYNC)
+using MANAGER = i2c::I2CAsyncManager<
+	i2c::I2CMode::FAST, i2c::I2CErrorPolicy::CLEAR_ALL_COMMANDS>;
+static MANAGER::I2CCOMMAND i2c_buffer[I2C_BUFFER_SIZE];
+#	else
+using MANAGER = i2c::I2CSyncManager<i2c::I2CMode::FAST>;
+#	endif
+#define DEBUG(OUT)
+#define RESET_DEBUG()
+
+#endif
+
+#if I2C_TRUE_ASYNC and not defined(FORCE_SYNC)
+REGISTER_I2C_ISR(MANAGER)
+#endif
+
+using streams::dec;
+using streams::hex;
+using streams::endl;
+
+using TOF = devices::vl53l0x::VL53L0X<MANAGER>;
+
+int main() __attribute__((OS_main));
+int main()
+{
+	board::init();
+	sei();
+
+	// open UART for traces
+	serial::hard::UATX<UART> uart{output_buffer};
+	streams::ostream out = uart.out();
+	uart.begin(115200);
+	out << streams::boolalpha;
+	out << F("Start") << endl;
+
+	// Initialize I2C async handler
+#ifdef DEBUG_I2C
+	DEBUGGER debugger;
+#endif
+
+#if I2C_TRUE_ASYNC and not defined(FORCE_SYNC)
+#	ifdef DEBUG_I2C
+	MANAGER manager{i2c_buffer, debugger, debugger};
+#	else
+	MANAGER manager{i2c_buffer};
+#	endif
+#else
+#	ifdef DEBUG_I2C
+	MANAGER manager{debugger, debugger};
+#	else
+	MANAGER manager;
+#	endif
+#endif
+
+	out << F("Instantiate VL53L0X") << endl;
+	TOF tof{manager};
+
+	out << F("Start I2C manager") << endl;
+	manager.begin();
+
+	out << F("Read VL53L0X status") << endl;
+	//TODO read all device info and display
+
+	uint8_t result = 0;
+	bool ok = tof.get_revision(result);
+	out << F("tof.get_revision(result) = ") << ok << F(", result = ") << hex << result << endl;
+	DEBUG(out);
+
+	ok = tof.get_model(result);
+	out << F("tof.get_model(result) = ") << ok << F(", result = ") << hex << result << endl;
+	DEBUG(out);
+
+	ok = tof.get_power_mode(result);
+	out << F("tof.get_power_mode(result) = ") << ok << F(", result = ") << hex << result << endl;
+	DEBUG(out);
+
+	ok = tof.get_range_status(result);
+	out << F("tof.get_range_status(result) = ") << ok << F(", result = ") << hex << result << endl;
+	DEBUG(out);
+
+	manager.end();
+}
