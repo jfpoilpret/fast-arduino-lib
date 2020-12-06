40d1be68efc7dcc0336c5dfe2201b88cebd854e4
jfpoilpret <jfpoilpret@gmail.com>
2020-12-07 00:10:29 +0100

    Check improvements work also on FakeFuture.

--- examples/experiments/improved_futures/Futures.cpp
+++ examples/experiments/improved_futures/Futures.cpp
@@ -37,6 +37,8 @@
 // Register vector for UART (used for debug)
 REGISTER_UATX_ISR(0)
 
+// #define REAL_FUTURE
+
 // Example starts here
 //=====================
 
@@ -47,15 +49,17 @@ using namespace containers;
 static constexpr const uint8_t OUTPUT_BUFFER_SIZE = 128;
 static char output_buffer[OUTPUT_BUFFER_SIZE];
 
-//TODO - example: self listening Future to change its INPUT based on OUTPUT
+//TODO - make on_change...() private and check if OK
+//TODO - perform checks on FuturesGroup
 
-// template<typename F, uint8_t SIZE> class FuturesGroup : public FutureListener<F>
+// template<typename F, uint8_t SIZE> class FuturesGroup
 // {
 // 	//TODO ensure F is a proper future (need specific traits...)
 // public:
 // 	//TODO how to deal with move constructor?
 // 	FutureStatus status() const
 // 	{
+// 		//TODO find status among all futures (or only last one?)
 // 		return status_;
 // 	}
 
@@ -84,48 +88,29 @@ static char output_buffer[OUTPUT_BUFFER_SIZE];
 // 		for (F& future: futures)
 // 			*temp++ = future;
 // 	}
-
-// 	void on_status_change(const F& future) override
-// 	{
-// 		const FutureStatus status = future.status();
-// 		switch (status)
-// 		{
-// 			case FutureStatus::ERROR:
-// 			error_ = future.error();
-// 			// Intentional Fallthrough
-
-// 			case FutureStatus::INVALID:
-// 			status_ = status;
-// 			break;
-
-// 			case FutureStatus::READY:
-// 			// check if all futures ready
-// 			if (++count_ready_ == SIZE)
-// 				status_ = FutureStatus::READY;
-// 			break;
-
-// 			case FutureStatus::NOT_READY:
-// 			// do nothing
-// 			break;
-// 		}
-// 	}
-
+// 
 // private:
 // 	F* futures_[SIZE];
 // 	uint8_t count_ready_ = 0;
-// 	volatile FutureStatus status_ = FutureStatus::NOT_READY;
-// 	int error_ = 0;
 // };
 
-struct FutureListener : FutureStatusListener<AbstractFuture>, FutureOutputListener<AbstractFuture>
+#ifdef REAL_FUTURE
+#define ABSTRACTFUTURE AbstractFuture
+#define FUTURE Future
+#else
+#define ABSTRACTFUTURE AbstractFakeFuture
+#define FUTURE FakeFuture
+#endif
+
+struct FutureListener : FutureStatusListener<ABSTRACTFUTURE>, FutureOutputListener<ABSTRACTFUTURE>
 {
 	explicit FutureListener(ostream& out) : out_{out} {}
 	
-	void on_status_change(UNUSED const AbstractFuture& future, FutureStatus new_status) override
+	void on_status_change(UNUSED const ABSTRACTFUTURE& future, FutureStatus new_status) override
 	{
 		out_ << F("on_status_change() status = ") << new_status << endl;
 	}
-	void on_output_change(UNUSED const AbstractFuture& future, uint8_t* output_data, uint8_t* output_current) override
+	void on_output_change(UNUSED const ABSTRACTFUTURE& future, uint8_t* output_data, uint8_t* output_current) override
 	{
 		out_	<< F("on_output_change() data = ") << hex << output_data << F(", current = ") 
 				<< hex << output_current << endl;
@@ -134,9 +119,9 @@ struct FutureListener : FutureStatusListener<AbstractFuture>, FutureOutputListen
 	ostream& out_;
 };
 
-class MyFuture : public Future<uint32_t, uint8_t>
+class MyFuture : public FUTURE<uint32_t, uint8_t>
 {
-	using PARENT = Future<uint32_t, uint8_t>;
+	using PARENT = FUTURE<uint32_t, uint8_t>;
 	static constexpr uint8_t REG_INDEX = 0x34;
 
 public:
@@ -154,9 +139,9 @@ struct UpdateRegister
 
 	uint8_t data[3] = {0 ,0, 0};
 };
-class UpdateRegisterFuture : public Future<uint8_t, UpdateRegister>, public FutureOutputListener<AbstractFuture>
+class UpdateRegisterFuture : public FUTURE<uint8_t, UpdateRegister>, public FutureOutputListener<ABSTRACTFUTURE>
 {
-	using PARENT = Future<uint8_t, UpdateRegister>;
+	using PARENT = FUTURE<uint8_t, UpdateRegister>;
 
 public:
 	UpdateRegisterFuture(uint8_t reg_index, uint8_t set_mask)
@@ -165,7 +150,7 @@ public:
 	UpdateRegisterFuture& operator=(UpdateRegisterFuture&&) = default;
 
 	void on_output_change(
-		UNUSED const AbstractFuture& future, UNUSED uint8_t* output_data, uint8_t* output_current) override
+		UNUSED const ABSTRACTFUTURE& future, UNUSED uint8_t* output_data, uint8_t* output_current) override
 	{
 		// Only one call expected, directly get output value and change it
 		uint8_t value = *(output_current - 1);
