c37ededbd48d520f5151b1883ee7c8e0a2aa2019
jfpoilpret <jfpoilpret@gmail.com>
2022-09-18 23:46:13 +0200

    Cleanup code and add comments

--- cores/fastarduino/devices/wiegand.h
+++ cores/fastarduino/devices/wiegand.h
@@ -21,7 +21,6 @@
 #ifndef WIEGAND_H
 #define WIEGAND_H
 
-#include <stdint.h>
 #include "../utilities.h"
 
 namespace devices
@@ -69,7 +68,7 @@ namespace devices::protocols
 
 		bool valid_() const
 		{
-			uint32_t data = convert(buffer_);
+			uint32_t data = data24_;
 
 			// 1. check parity on first 12 bits
 			if (parity1_ != parity(PARITY1_HIGH_BIT_INDEX, data)) return false;
@@ -82,7 +81,7 @@ namespace devices::protocols
 
 		DATA_TYPE get_data_() const
 		{
-			return convert(buffer_);
+			return data24_;
 		}
 
 		void on_falling_data0()
@@ -149,11 +148,6 @@ namespace devices::protocols
 		static constexpr uint8_t INITIAL_MASK = 0x80;
 		static constexpr uint8_t DATA_BYTES = DATA_BITS / 8;
 
-		static uint32_t convert(uint32_t buffer)
-		{
-			return buffer & 0x00FFFFFFUL;
-		}
-
 		static bool parity(uint8_t start, uint32_t buffer)
 		{
 			uint8_t count = 0;
@@ -169,16 +163,29 @@ namespace devices::protocols
 
 		union
 		{
+			// Used to clear all in one instruction
 			uint32_t buffer_;
 			struct
 			{
+				// Used to store bits, byte after byte
 				uint8_t data_[DATA_BYTES];
+				// Count of received bits (including parity bits)
 				uint8_t count_;
 			};
+			struct
+			{
+				// Used to access the actual 24-bits data
+				uint32_t data24_ : DATA_BITS;
+				uint32_t :8;
+			};
 		};
+		// Points to current read byte in data_[]
 		uint8_t* current_;
+		// Mask for current read bit
 		uint8_t mask_;
+		// First parity bit read (even)
 		bool parity1_;
+		// Last parity bit read (odd)
 		bool parity2_;
 	};
 }
