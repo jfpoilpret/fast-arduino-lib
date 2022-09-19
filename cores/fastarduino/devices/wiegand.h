e03df3437b7988277903538728d8df72440bea87
jfpoilpret <jfpoilpret@gmail.com>
2022-09-19 10:36:49 +0200

    Add API doc to Wiegand class.

--- cores/fastarduino/devices/wiegand.h
+++ cores/fastarduino/devices/wiegand.h
@@ -35,25 +35,77 @@ namespace devices
 
 namespace devices::protocols
 {
-	//TODO DOCS
-	//TODO study Wiegand protocol extensions and improve this class as a template (on enum defining all Wiegand protocols)
-	//TODO may need traits for generic use of extended protocols
+	/**
+	 * Supporting class for the [Wiegand 26 bits protocol](https://en.wikipedia.org/wiki/Wiegand_interface)
+	 * (used by many access control devices).
+	 * 
+	 * To use Wiegand class, you just need to instantiate it in your own device class,
+	 * ensure you call `on_falling_data0()` and `on_falling_data1()` when you detect 
+	 * (ISR preeferred) that line DATA0 or DATA1 is falling to 0V.
+	 * 
+	 * Other API methods allow you to know the state of reading (finished, parity valid),
+	 * get the read data (if available and valid) and reset the reading.
+	 * 
+	 * @note only the "original" Wiegand interface standard (26-bits, including 2
+	 * parity bits) is supported here; there are many variations in terms of bits 
+	 * number and parity checks, developped by access control companies, but it is
+	 * not clear if some have been standardized. If some needs occur for extended
+	 * support in the future, it is probable this class will be refactored to
+	 * support these extensions.
+	 */
 	class Wiegand
 	{
 	public:
+		/** 
+		 * The data type used to return data read from the access control device.
+		 * This may be larger than what is actually needed.
+		 */
 		using DATA_TYPE = uint32_t;
+
+		/** 
+		 * The actual number of bits of data in `DATA_TYPE`. This does not include
+		 * parity bits which do not hold any access control data.
+		 * The actual bits are always the LSB in `DATA_TYPE`, other bits (HSB)
+		 * are set to `0`.
+		 */
 		static constexpr uint8_t DATA_BITS = 24;
 
+		/**
+		 * Construct a new Wiegand instance to be sued by an access control device.
+		 * The instance is ready to be used for data reading.
+		 */
 		Wiegand()
 		{
 			reset_();
 		}
 
+		/**
+		 * Reset current read state of this instance. Any bits read will be lost.
+		 * You should call this method after having read latest data with `get_data_()`.
+		 * 
+		 * This method is synchronized, hence you can call it from an
+		 * an interrupt-unsafe context; if you are sure you are in an interrupt-safe,
+		 * you should use the not synchronized flavor `reset_()` instead.
+		 * 
+		 * @sa reset_()
+		 * @sa get_data_()
+		 */
 		void reset()
 		{
 			synchronized reset_();
 		}
 
+		/**
+		 * Reset current read state of this instance. Any bits read will be lost.
+		 * You should call this method after having read latest data with `get_data_()`.
+		 * 
+		 * This method is not synchronized, hence you must ensure it is called from
+		 * an interrupt-safe context; otherwise, you should use the synchronized flavor
+		 * `reset_()` instead.
+		 * 
+		 * @sa reset_()
+		 * @sa get_data_()
+		 */
 		void reset_()
 		{
 			buffer_ = 0UL;
@@ -61,11 +113,34 @@ namespace devices::protocols
 			mask_ = INITIAL_MASK;
 		}
 
+		/**
+		 * Check if data is available, i\.e\. all 26 bits have been received already.
+		 * 
+		 * @note this method does not care about validity of received bits (parity check),
+		 * for this you will need to also call `valid_()`.
+		 * 
+		 * This method is not synchronized, hence you must ensure it is called from
+		 * an interrupt-safe context.
+		 * 
+		 * @sa valid_()
+		 */
 		bool available_() const
 		{
 			return count_ == FRAME_BITS;
 		}
 
+		/**
+		 * Check if current data is valid, i\.e\. parity has been checked against 
+		 * both even and odd parity bits (as per Wiegand 26 bits standard).
+		 * 
+		 * @note this method does not care about availability of data (all 26 bits
+		 * received or not yet), for this you will need to first call `available_()`.
+		 * 
+		 * This method is not synchronized, hence you must ensure it is called from
+		 * an interrupt-safe context.
+		 * 
+		 * @sa available_()
+		 */
 		bool valid_() const
 		{
 			uint32_t data = data24_;
@@ -79,11 +154,33 @@ namespace devices::protocols
 			return true;
 		}
 
+		/**
+		 * Get data read from access control device.
+		 * 
+		 * @note this method does not care about availability or validity of data,
+		 * it will just return the current state of data received so far. This is why
+		 * you should first call `available_()` and `valid_()` before calling `get_data_()`.
+		 * 
+		 * This method is not synchronized, hence you must ensure it is called from
+		 * an interrupt-safe context.
+		 * 
+		 * @sa available_()
+		 * @sa valid_()
+		 */
 		DATA_TYPE get_data_() const
 		{
 			return data24_;
 		}
 
+		/**
+		 * Your device shall call this method whenever DATA0 line level is falling 
+		 * to `0`, which means a `0` bit must be added to read data.
+		 * 
+		 * It is expected to work with interrupts (EXT or PCI) for connecting
+		 * the access control device DATA0 and DATA1 pins.
+		 * 
+		 * @sa on_falling_data1()
+		 */
 		void on_falling_data0()
 		{
 			// This is a 0
@@ -108,6 +205,15 @@ namespace devices::protocols
 			++count_;
 		}
 
+		/**
+		 * Your device shall call this method whenever DATA1 line level is falling 
+		 * to `0`, which means a `1` bit must be added to read data.
+		 * 
+		 * It is expected to work with interrupts (EXT or PCI) for connecting
+		 * the access control device DATA0 and DATA1 pins.
+		 * 
+		 * @sa on_falling_data0()
+		 */
 		void on_falling_data1()
 		{
 			// This is a 1
