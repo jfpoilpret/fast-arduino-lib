45d55ff0558d1faad9ef2c847dac5c0aad1f793f
jfpoilpret <jfpoilpret@gmail.com>
2023-04-30 23:18:24 +0200

    Refactor to use one single Python file for font editing.

--- tools/font-editor/FontEditor.py
+++ tools/font-editor/FontEditor.py
@@ -21,7 +21,6 @@
 
 #TODO 1h+	Move edited glyph (up/down/left/right) -> difficulty will be UNDO
 #TODO 4h	Generate horizontal fonts too
-#TODO 30'	Refactor to put exporting functions here too (only one source code file)
 
 from dataclasses import dataclass
 import pickle
@@ -30,8 +29,6 @@ import re
 from tkinter import *
 from tkinter import filedialog, messagebox
 from tkinter import ttk
-
-from font_export import generate_fastarduino_header, generate_fastarduino_source, generate_regular_code
 
 MIN_CHAR_CODE = 32
 MAX_CHAR_CODE = 127
@@ -763,6 +760,139 @@ class FontEditor(ttk.Frame):
 			self.font_state.update_size(width=size[0], height=size[1])
 			self.set_font(font_state=self.font_state)
 			self.is_dirty = True
+
+FASTARDUINO_HEADER_LICENSE = """//   Copyright 2016-2023 Jean-Francois Poilpret
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+"""
+
+FASTARDUINO_HEADER_TEMPLATE = FASTARDUINO_HEADER_LICENSE + """
+/// @cond api
+
+#ifndef {font_header_define}
+#define {font_header_define}
+
+#include "../font.h"
+
+namespace devices::display
+{{
+	class {font_name} : public Font<{vertical}>
+	{{
+	public:
+		{font_name}() : Font{{0x{first_char:02x}, 0x{last_char:02x}, {font_width}, {font_height}, FONT}} {{}}
+
+	private:
+		static const uint8_t FONT[] PROGMEM;
+	}};
+}}
+#endif /* {font_header_define} */
+
+/// @endcond
+"""
+
+FASTARDUINO_SOURCE_TEMPLATE = FASTARDUINO_HEADER_LICENSE + """
+#include "{font_header}"
+
+const uint8_t devices::display::{font_name}::FONT[] PROGMEM =
+{{
+{font_glyphs}}};
+"""
+
+REGULAR_CODE_TEMPLATE = """
+#include <fastarduino/devices/font.h>
+
+class {font_name} : public devices::display::Font<{vertical}>
+{{
+public:
+	{font_name}() : Font{{0x{first_char:02x}, 0x{last_char:02x}, {font_width}, {font_height}, FONT}} {{}}
+
+private:
+	static const uint8_t FONT[] PROGMEM;
+}};
+
+const uint8_t {font_name}::FONT[] PROGMEM =
+{{
+{font_glyphs}}};
+"""
+
+#FIXME if glyph_car is \ then it shall be escaped!
+GLYPH_TEMPLATE = """	{glyph_row}	// 0x{glyph_code:02x} {glyph_char}
+"""
+
+def generate_fastarduino_header(filename: str, font_name: str, width: int, height: int, 
+	first_char: int, last_char: int, vertical: bool) -> str:
+	# generate header as string
+	return FASTARDUINO_HEADER_TEMPLATE.format(
+		font_header_define = filename.upper() + '_HH',
+		font_name = font_name,
+		vertical = 'true' if vertical else 'false',
+		first_char = first_char,
+		last_char = last_char,
+		font_width = width,
+		font_height =  height)
+
+def generate_glyph_rows(c: int, width: int, height: int, vertical: bool, glyph: list[list[bool]]):
+	glyph_rows = ''
+	if vertical:
+		for row in range(int((height - 1) / 8 + 1)):
+			glyph_row = ''
+			for col in range(width):
+				mask = 1
+				byte = 0
+				for i in range(8):
+					if row * 8 + i == height:
+						break
+					# print(f"glyph[{row * 8 + i}][{col}]")
+					if glyph[row * 8 + i][col]:
+						byte |= mask
+					mask *= 2
+				glyph_row += f'0x{byte:02x}, '
+			glyph_rows += GLYPH_TEMPLATE.format(glyph_row = glyph_row, glyph_code = c, glyph_char = chr(c))
+	else:
+		#TODO
+		pass
+	return glyph_rows
+
+def generate_all_glyphs(width: int, height: int, first_char: int, last_char: int, 
+	vertical: bool, glyphs: dict[int, list[list[bool]]]) -> str:
+	# First generate all rows for glyphs definition
+	all_glyphs = ''
+	for c in range(first_char, last_char + 1):
+		glyph = glyphs[c]
+		all_glyphs += generate_glyph_rows(c, width, height, vertical, glyph)
+	return all_glyphs
+
+def generate_fastarduino_source(filename: str, font_name: str, width: int, height: int, 
+	first_char: int, last_char: int, vertical: bool, glyphs: dict[int, list[list[bool]]]) -> str:
+	# First generate all rows for glyphs definition
+	all_glyphs = generate_all_glyphs(width, height, first_char, last_char, vertical, glyphs)
+	return FASTARDUINO_SOURCE_TEMPLATE.format(
+		font_header = filename + '.h',
+		font_name = font_name,
+		font_glyphs = all_glyphs)
+
+def generate_regular_code(filename: str, font_name: str, width: int, height: int, 
+	first_char: int, last_char: int, vertical: bool, glyphs: dict[int, list[list[bool]]]) -> str:
+	# First generate all rows for glyphs definition
+	all_glyphs = generate_all_glyphs(width, height, first_char, last_char, vertical, glyphs)
+	return REGULAR_CODE_TEMPLATE.format(
+		font_name = font_name,
+		vertical = 'true' if vertical else 'false',
+		first_char = first_char,
+		last_char = last_char,
+		font_width = width,
+		font_height =  height,
+		font_glyphs = all_glyphs)
 
 if __name__ == '__main__':
 	# Create Window
