4b88a05ed01078739f50a8760832943472aa7345
jfpoilpret <jfpoilpret@gmail.com>
2023-05-21 10:57:16 +0200

    Add small horizontal font for Arduino LCD tests

---
+++ cores/fastarduino/devices/fonts/font_h8x12_default.h
@@ -0,0 +1,35 @@
+//   Copyright 2016-2023 Jean-Francois Poilpret
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+/// @cond api
+
+#ifndef FONT_H8X12_DEFAULT_HH
+#define FONT_H8X12_DEFAULT_HH
+
+#include "../font.h"
+
+namespace devices::display
+{
+	class DefaultFont12x8 : public Font<false>
+	{
+	public:
+		DefaultFont12x8() : Font{0x20, 0x7e, 8, 12, FONT} {}
+
+	private:
+		static const uint8_t FONT[] PROGMEM;
+	};
+}
+#endif /* FONT_H8X12_DEFAULT_HH */
+
+/// @endcond
