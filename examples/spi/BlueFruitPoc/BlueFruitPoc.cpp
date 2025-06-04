ee237557bddf215863441125abc280487cf9e402
jfpoilpret <jean-francois@poilpret.ch>
2025-06-04 22:11:10 +0200

    Rework errors; prepare ISR implementation

--- examples/spi/BlueFruitPoc/BlueFruitPoc.cpp
+++ examples/spi/BlueFruitPoc/BlueFruitPoc.cpp
@@ -30,7 +30,9 @@
  */
 
 #include <fastarduino/gpio.h>
+#include <fastarduino/int.h>
 #include <fastarduino/time.h>
+#include <fastarduino/timer.h>
 #include <fastarduino/spi.h>
 #include <fastarduino/uart.h>
 #include <fastarduino/utilities.h>
@@ -55,23 +57,20 @@ static constexpr const spi::ClockRate CLOCK_RATE = spi::compute_clockrate(SPI_CL
 
 // For testing we use default SS pin as CS
 static constexpr const board::DigitalPin CS = board::DigitalPin::D10_PB2;
-static constexpr const board::DigitalPin IRQ = board::DigitalPin::D7_PD7;
-static constexpr const board::DigitalPin RESET = board::DigitalPin::D5_PD5;
+#define IRQ_PIN 0
+static constexpr const board::ExternalInterruptPin IRQ = board::ExternalInterruptPin::D2_PD2_EXT0;
+static constexpr const board::DigitalPin RESET = board::DigitalPin::D6_PD6;
 
-//TODO FINAL DESIGN
-//TODO - add istream/ostream for command/UATX/UARX?
-//TODO - support data/command mode?
-//TODO - automatically add events support for everything (connect/disconnect, UART, GATT notifications...)
-//TODO		but dispatch conditionally (if callback registered or not)? HOW?
-//TODO - support asynchronous calls (limit time inside active methods!)
-//TODO - use Futures and RTT (because this BLE device is incredibly sloooooooow!)
+// Timer 16 bit for ticks every 60us or so
+#define NTIMER 1
+static constexpr const board::Timer BLE_TIMER = board::Timer::TIMER1;
 
 //TODO POC
-// 15'	- fine tune timing for CS and 1st byte retry
 // 2h+	- add ISR for IRQ and Timer (CTC for 1st byte transmit retry)
-// 30'	- better error handling (enum class and specific error() method)
+// 15'	- better error handling (specific error() method)
 // 2h+	- implement GATT API (how?) with callbacks: use Builder pattern?
 // 2h+	- implement UART API (to be inferred thoroughly first)
+//			- support data/command mode?
 // 1h	- implement "basic" commands (e.g. factory reset)
 // 1h	- add ostream for AT commands (how to trigger sending?)
 // 1h	- add istream for AT responses (how to trigger end of reception?)
@@ -79,6 +78,19 @@ static constexpr const board::DigitalPin RESET = board::DigitalPin::D5_PD5;
 // Subclass SPIDevice to make protected methods available from main()
 class PublicDevice: public spi::SPIDevice<CS, CHIP_SELECT, CLOCK_RATE, MODE, DATA_ORDER>
 {
+public:
+	enum class Error : uint8_t
+	{
+		OK = 0,
+		BUSY,
+		TIMEOUT_DEVICE_NOT_READY,
+		TIMEOUT_IRQ,
+		TIMEOUT_DEVICE_NO_DATA,
+		RESPONSE_WITH_ERROR,
+		RESPONSE_SIZE_OVERFLOW,
+	};
+
+private:
 	// Events bits for AT+EVENTENABLE and other event-related AT commands
 	static constexpr const uint8_t EVENT_CONNECT = 0;
 	static constexpr const uint8_t EVENT_DISCONNECT = 1;
@@ -90,14 +102,12 @@ class PublicDevice: public spi::SPIDevice<CS, CHIP_SELECT, CLOCK_RATE, MODE, DAT
 	static constexpr const uint16_t DELAY_RESET_CLEAR_MS = 10;
 	static constexpr const uint16_t DELAY_AFTER_RESET_MS = 1000;
 
-	// Retry times in loops (for 1st byte OK and for IRQ wait)
+	// Retry times (in microseconds) in loops (for 1st byte OK and for IRQ wait)
 	static constexpr const uint16_t DELAY_BEFORE_CS_US = 60;
-	//TODO reduce (or remove) this extra delay as much as possible!
-	// static constexpr const uint16_t DELAY_AFTER_CS_US = 10;
 	static constexpr const uint16_t DELAY_AFTER_CS_US = 0;
 	static constexpr const uint16_t DELAY_LOOP_IRQ_US = 10;
 
-	// Timeouts for aborting waits on 1st byte OK or received IRQ 
+	// Timeouts (in microseconds) for aborting waits on 1st byte OK or received IRQ 
 	static constexpr const uint16_t TIMEOUT_DEVICE_READY_US = 1000;
 	static constexpr const uint16_t TIMEOUT_IRQ_WAIT_US = 10000;
 
@@ -119,6 +129,17 @@ class PublicDevice: public spi::SPIDevice<CS, CHIP_SELECT, CLOCK_RATE, MODE, DAT
 	// SDEP payload size masks
 	static constexpr const uint8_t MASK_MORE_DATA = 0x80;
 	static constexpr const uint8_t MASK_PAYLOAD_SIZE = 0x1F;
+
+	// Status of an exchange operation
+	enum class OperationStatus : uint8_t
+	{
+		NO_OP = 0,
+		SENDING_TYPE,
+		SENDING_ALL,
+		WAITING_IRQ,
+		RECEIVING_TYPE,
+		RECEIVING_ALL
+	};
 
 	struct SDEPHeader
 	{
@@ -177,12 +198,12 @@ class PublicDevice: public spi::SPIDevice<CS, CHIP_SELECT, CLOCK_RATE, MODE, DAT
 		}
 	}
 
-	uint8_t send_packet(const SDEPPacket& packet)
+	Error send_packet(const SDEPPacket& packet)
 	{
 		// Start SPI transfer, send message type and ensure we get a non error return byte (0xFE)
 		uint8_t type = wait_for_type(packet.header.type, loop_count_for_cs(TIMEOUT_DEVICE_READY_US), true);
 		if (type == DEVICE_NOT_READY)
-			return ERROR_TIMEOUT_DEVICE_NOT_READY;
+			return Error::TIMEOUT_DEVICE_NOT_READY;
 	
 		// Command ID
 		transfer(utils::low_byte(packet.header.command));
@@ -195,16 +216,25 @@ class PublicDevice: public spi::SPIDevice<CS, CHIP_SELECT, CLOCK_RATE, MODE, DAT
 		transfer(packet.payload, packet.header.size & MASK_PAYLOAD_SIZE);
 	
 		end_transfer();
-		return OK;
+		return Error::OK;
 	}
 
-	uint8_t get_packet(SDEPPacket& packet)
+	Error get_packet(SDEPPacket& packet)
 	{
 		// Start SPI transfer, send message type and ensure we get a non error return byte (0xFE)
 		uint8_t type = wait_for_type(0xFF, loop_count_for_cs(TIMEOUT_DEVICE_READY_US), false);
 		packet.header.type = type;
-		if (type == DEVICE_NOT_READY || type == DEVICE_NO_DATA)
-			return type;
+		switch (type)
+		{
+			case DEVICE_NOT_READY:
+			return Error::TIMEOUT_DEVICE_NOT_READY;
+
+			case DEVICE_NO_DATA:
+			return Error::TIMEOUT_DEVICE_NO_DATA;
+			
+			default:
+			break;
+		}
 	
 		// Command ID
 		uint8_t low = transfer(0xFF);
@@ -219,17 +249,10 @@ class PublicDevice: public spi::SPIDevice<CS, CHIP_SELECT, CLOCK_RATE, MODE, DAT
 		transfer(packet.payload, size & MASK_PAYLOAD_SIZE, 0xFF);
 	
 		end_transfer();
-		return OK;
+		return Error::OK;
 	}
 
 public:
-	static constexpr const uint8_t OK = 0;
-	static constexpr const uint8_t ERROR_TIMEOUT_IRQ = 1;
-	static constexpr const uint8_t ERROR_TIMEOUT_DEVICE_NOT_READY = 2;
-	static constexpr const uint8_t ERROR_TIMEOUT_DEVICE_NO_DATA = 3;
-	static constexpr const uint8_t ERROR_RESPONSE = 4;
-	static constexpr const uint8_t ERROR_SIZE_OVERFLOW = 5;
-	
 	static constexpr uint16_t loop_count_for_cs(uint16_t timeout_us)
 	{
 		return timeout_us / (DELAY_BEFORE_CS_US + DELAY_AFTER_CS_US);
@@ -239,25 +262,35 @@ public:
 		return timeout_us / DELAY_LOOP_IRQ_US;
 	}
 
-	PublicDevice(): SPIDevice() {}
+	PublicDevice(): SPIDevice()
+	{
+		// Register callback handlers
+		interrupt::register_handler(*this);
+		// Start timer but suspend interrupts
+		synchronized
+		{
+			timer_.begin_(COUNTER);
+			timer_.suspend_interrupts_();
+		}
+	}
 
 	// Functions for events handling
-	void register_callbacks()
-	{
-		// Register events connect/disconnect
-		await_at_command("AT+EVENTENABLE=0x3");
-	}
+	// void register_callbacks()
+	// {
+	// 	// Register events connect/disconnect
+	// 	await_at_command("AT+EVENTENABLE=0x3");
+	// }
 
-	bool check_events()
-	{
-		static constexpr const uint8_t SIZE = 64;
-		char response[SIZE+1];
-		bool ok = await_at_command("AT+EVENTSTATUS", response, SIZE);
-		if (!ok) return false;
-		//TODO Need to get response and check if event is present
-		// First line contains XX,YY (2 hex numbers that are bitfields, 
-		// XX is for system events and YY for GATT events)
-	}
+	// bool check_events()
+	// {
+	// 	static constexpr const uint8_t SIZE = 64;
+	// 	char response[SIZE+1];
+	// 	bool ok = await_at_command("AT+EVENTSTATUS", response, SIZE);
+	// 	if (!ok) return false;
+	// 	//TODO Need to get response and check if event is present
+	// 	// First line contains XX,YY (2 hex numbers that are bitfields, 
+	// 	// XX is for system events and YY for GATT events)
+	// }
 
 	void reset()
 	{
@@ -267,27 +300,27 @@ public:
 		time::delay_ms(DELAY_AFTER_RESET_MS);
 	}
 
-	uint8_t await_at_command(const char* command)
+	Error await_at_command(const char* command)
 	{
 		static constexpr const uint8_t SIZE = 64;
 		char response[SIZE+1];
 		return await_at_command(command, response, SIZE);
 	}
 
-	uint8_t await_at_command(const char* command, char* response, uint8_t size)
+	Error await_at_command(const char* command, char* response, uint8_t size)
 	{
-		uint8_t error = send_command(command);
-		if (error != OK)
+		Error error = send_command(command);
+		if (error != Error::OK)
 			return error;
 		// reduce size to keep final string 0
 		--size;
 		error = get_response((uint8_t*) response, size);
-		if (error != OK) return error;
+		if (error != Error::OK) return error;
 		response[size] = 0;
 		return check_ok(response);
 	}
 
-	uint8_t send_command(const char* command)
+	Error send_command(const char* command)
 	{
 		// Prepare count of packets
 		const uint8_t len = strlen(command);
@@ -300,36 +333,36 @@ public:
 			const bool last = (num_packet == count_packets - 1);
 			const uint8_t packet_size = (last && (last_packet_size != 0) ? last_packet_size : SDEP_PAYLOAD_SIZE);
 			SDEPPacket packet{TYPE_COMMAND, COMMAND_AT, packet_size, !last, payload};
-			uint8_t error = send_packet(packet);
-			if (error != OK)
+			Error error = send_packet(packet);
+			if (error != Error::OK)
 				return error;
 			payload += packet_size;
 		}
-		return OK;
+		return Error::OK;
 	}
 
-	uint8_t wait_for_irq(uint16_t loop_count)
+	Error wait_for_irq(uint16_t loop_count)
 	{
 		while (!irq_.value())
 		{
 			if (loop_count-- == 0)
-				return ERROR_TIMEOUT_IRQ;
+				return Error::TIMEOUT_IRQ;
 			time::delay_us(DELAY_LOOP_IRQ_US);
 		}
-		return OK;
+		return Error::OK;
 	}
 
-	uint8_t check_ok(const char* response)
+	Error check_ok(const char* response)
 	{
 		char* found = strstr(response, "OK\r\n");
-		return (found != 0 ? OK : ERROR_RESPONSE);
+		return (found != 0 ? Error::OK : Error::RESPONSE_WITH_ERROR);
 	}
 
 	//TODO add expected command value?
-	uint8_t get_response(uint8_t* response, uint8_t& max_size)
+	Error get_response(uint8_t* response, uint8_t& max_size)
 	{
-		uint8_t error = wait_for_irq(loop_count_for_irq(TIMEOUT_IRQ_WAIT_US));
-		if (error != OK)
+		Error error = wait_for_irq(loop_count_for_irq(TIMEOUT_IRQ_WAIT_US));
+		if (error != Error::OK)
 			return error;
 
 		// Read all packet until last one
@@ -339,12 +372,12 @@ public:
 		{
 			SDEPPacket packet;
 			error = get_packet(packet);
-			if (error != OK)
+			if (error != Error::OK)
 				return error;
 			// Copy payload part to response
 			uint8_t size = packet.header.size & MASK_PAYLOAD_SIZE;
 			if (total + size > max_size)
-				return ERROR_SIZE_OVERFLOW;
+				return Error::RESPONSE_SIZE_OVERFLOW;
 			memcpy(current, packet.payload, size);
 			current += size;
 			total += size;
@@ -353,18 +386,81 @@ public:
 				break;
 		}
 		max_size = total;
-		return OK;
+		return Error::OK;
 	}
 
-	gpio::FAST_PIN<IRQ> irq_ = gpio::FAST_PIN<IRQ>{gpio::PinMode::INPUT};
+	void on_irq_high()
+	{
+		//TODO
+	}
+
+	void on_timer_compare()
+	{
+		//TODO
+	}
+
+	volatile OperationStatus operation_ = OperationStatus::NO_OP;
+
+	using TIMER = timer::Timer<BLE_TIMER>;
+	using CALC = TIMER::CALCULATOR;
+	static constexpr const TIMER::PRESCALER PRESCALER = CALC::CTC_prescaler(DELAY_BEFORE_CS_US);
+	static_assert(CALC::is_adequate_for_CTC(PRESCALER, DELAY_BEFORE_CS_US));
+	static constexpr const uint16_t COUNTER = CALC::CTC_counter(PRESCALER, DELAY_BEFORE_CS_US);
+	TIMER timer_ = TIMER{timer::TimerMode::CTC, PRESCALER, timer::TimerInterrupt::OUTPUT_COMPARE_A};
+
+	gpio::FAST_EXT_PIN<IRQ> irq_ = gpio::FAST_EXT_PIN<IRQ>{gpio::PinMode::INPUT};
+	interrupt::INTSignal<IRQ> signal_ = interrupt::INTSignal<IRQ>{interrupt::InterruptTrigger::RISING_EDGE};
 	gpio::FAST_PIN<RESET> reset_ = gpio::FAST_PIN<RESET>{gpio::PinMode::OUTPUT, true};
+
+	DECL_TIMER_COMP_FRIENDS;
+	DECL_INT_ISR_FRIENDS;
 	friend int main();
 };
+
+// REGISTER_INT_ISR_METHOD(IRQ_PIN, IRQ, PublicDevice, &PublicDevice::on_irq_high)
+// REGISTER_TIMER_COMPARE_ISR_METHOD(NTIMER, PublicDevice, &PublicDevice::on_timer_compare)
 
 using streams::endl;
 using streams::flush;
 using streams::dec;
 using streams::hex;
+using Error = PublicDevice::Error;
+
+streams::ostream& operator<<(streams::ostream& o, Error error)
+{
+	const flash::FlashStorage* label;
+	switch (error)
+	{
+		case Error::OK:
+		label = F("OK");
+		break;
+		
+		case Error::BUSY:
+		label = F("BUSY");
+		break;
+		
+		case Error::TIMEOUT_DEVICE_NOT_READY:
+		label = F("TIMEOUT_DEVICE_NOT_READY");
+		break;
+		
+		case Error::TIMEOUT_IRQ:
+		label = F("TIMEOUT_IRQ");
+		break;
+		
+		case Error::TIMEOUT_DEVICE_NO_DATA:
+		label = F("TIMEOUT_DEVICE_NO_DATA");
+		break;
+		
+		case Error::RESPONSE_SIZE_OVERFLOW:
+		label = F("RESPONSE_SIZE_OVERFLOW");
+		break;
+		
+		case Error::RESPONSE_WITH_ERROR:
+		label = F("RESPONSE_WITH_ERROR");
+		break;
+	}
+	return o << label;
+}
 
 int main()
 {
@@ -383,7 +479,6 @@ int main()
 	PublicDevice device;
 	device.reset();
 	out << F("SPI & device initialized") << endl;
-	// time::delay_ms(1);
 	
 	// // Try to send a minimal SDEP packet
 	// bool ok = device.send_command("ATZ");
@@ -403,8 +498,8 @@ int main()
 	// out << endl;
 
 	// Enable system events
-	uint8_t error = device.await_at_command("AT+EVENTENABLE=0x3");
-	if (error != PublicDevice::OK)
+	Error error = device.await_at_command("AT+EVENTENABLE=0x3");
+	if (error != Error::OK)
 		out << F("ERROR during await_at_command('AT+EVENTENABLE=0x3'): ") << error << endl;
 	// Loop to check connect/disconnect events status
 	while (true)
@@ -413,7 +508,7 @@ int main()
 		static constexpr const uint8_t SIZE = 64;
 		char response[SIZE+1];
 		error = device.await_at_command("AT+EVENTSTATUS", response, SIZE);
-		if (error != PublicDevice::OK)
+		if (error != Error::OK)
 			out << F("ERROR during await_at_command('AT+EVENTSTATUS'): ") << error << endl;
 		out << F("EVENTSTATUS = ") << response << flush;
 	}
